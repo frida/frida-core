dbe8a98c54383bf68758dba4abd2c68d1f52e7f4
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2010-05-05 20:21:06 +0200

    Winjector x64 test green!

--- src/service/winjector-helper-core.c
+++ src/service/winjector-helper-core.c
@@ -191,10 +191,23 @@ typedef struct _WorkerContext WorkerContext;
 
 struct _TrickContext
 {
+  /* These fields must be addressable with int8 --> */
   gpointer virtual_alloc_impl;
+  LPVOID virtual_alloc_arg_address;
+  SIZE_T virtual_alloc_arg_size;
+  DWORD virtual_alloc_arg_allocation_type;
+  DWORD virtual_alloc_arg_protect;
+
+  LPVOID memcpy_source;
+  SIZE_T memcpy_size;
+
+  SIZE_T worker_ctx_size;
+
   gpointer create_thread_impl;
+
   gpointer close_handle_impl;
 
+  /* Remaining fields must be addressable with uint8 --> */
   gsize saved_xax;
   gsize saved_xbx;
   gsize saved_xcx;
@@ -223,6 +236,7 @@ struct _WorkerContext
 {
   gpointer load_library_impl;
   gpointer virtual_free_impl;
+  gpointer exit_thread_impl;
 
   WCHAR dll_path[MAX_PATH + 1];
 };
@@ -233,91 +247,189 @@ trick_thread_into_loading_dll (HANDLE process_handle, HANDLE thread_handle,
 {
   HMODULE kmod;
   guint8 trick_code[] = {
+#ifdef _M_X64
+    /*
+     * Align stack on a 16 byte boundary
+     */
+    0x48, 0x31, 0xC0,                                                             /* xor rax, rax */
+    0x48, 0xFF, 0xC8,                                                             /* dec rax      */
+    0x48, 0xC1, 0xE0, 0x04,                                                       /* shl rax, 4   */
+    0x48, 0x21, 0xC4,                                                             /* and rsp, rax */
+
+    /*
+     * Reserve stack space for arguments (48), rounded up to next 16 byte boundary (64)
+     */
+    0x48, 0x83, 0xEC, 64,                                                         /* sub rsp, 64 */
+
     /*
      * guint8 * worker_data = VirtualAlloc (...);
      */
-    0x6A, 0x40,             /* push PAGE_EXECUTE_READWRITE        (flProtect) */
-    0x68,                   /* push MEM_COMMIT|MEM_RESERVE (flAllocationType) */
-    0x00, 0x30, 0x00, 0x00,
-    0x68,                   /* push 0xDDCCBBAA                       (dwSize) */
-    /* offset 8: */ 0xAA, 0xBB, 0xCC, 0xDD,
-    0x6A, 0x00,             /* push 0                             (lpAddress) */
-    0xFF, 0x53, offsetof (TrickContext, virtual_alloc_impl),          /* call */
+    0x44, 0x8B, 0x4B, offsetof (TrickContext, virtual_alloc_arg_protect),         /* mov r9d, [rbx + 0xAA] */
+    0x44, 0x8B, 0x43, offsetof (TrickContext, virtual_alloc_arg_allocation_type), /* mov r8d, [rbx + 0xAA] */
+    0x48, 0x8B, 0x53, offsetof (TrickContext, virtual_alloc_arg_size),            /* mov rdx, [rbx + 0xAA] */
+    0x48, 0x8B, 0x4B, offsetof (TrickContext, virtual_alloc_arg_address),         /* mov rcx, [rbx + 0xAA] */
+    0xFF, 0x53,       offsetof (TrickContext, virtual_alloc_impl),                /* call     [rbx + 0xAA] */
 
     /*
      * memcpy (worker_data, ctx->worker_data, sizeof (ctx->worker_data));
      */
-    0x89, 0xC7,                           /* mov edi, eax                     */
-    0x8D, 0xB3,                           /* lea esi, [ebx + 0xDDCCBBAA]      */
-    /* offset 21: */ 0xAA, 0xBB, 0xCC, 0xDD,
-    0xB9,                                 /* mov ecx, 0xDDCCBBAA              */
-    /* offset 26: */ 0xAA, 0xBB, 0xCC, 0xDD,
-    0xFC,                                 /* cld                              */
-    0xF3, 0xA5,                           /* rep movsd                        */
+    0x48, 0x89, 0xC7,                                                             /* mov rdi, rax          */
+    0x48, 0x8B, 0x73, offsetof (TrickContext, memcpy_source),                     /* mov rsi, [rbx + 0xAA] */
+    0x48, 0x8B, 0x4B, offsetof (TrickContext, memcpy_size),                       /* mov rcx, [rbx + 0xAA] */
+    0xFC,                                                                         /* cld                   */
+    0xF3, 0xA4,                                                                   /* rep movsb             */
 
     /*
      * HANDLE worker_thread = CreateThread (...);
      */
-    0x6A, 0x00,                           /* push NULL           (lpThreadId) */
-    0x6A, 0x00,                           /* push 0         (dwCreationFlags) */
-    0x50,                                 /* push eax           (lpParameter) */
-    0x05,                                 /* add eax, 0xDDCCBBAA              */
-    /* offset 39: */ 0xAA, 0xBB, 0xCC, 0xDD,
-    0x50,                                 /* push eax        (lpStartAddress) */
-    0x6A, 0x00,                           /* push 0             (dwStackSize) */
-    0x6A, 0x00,                           /* push NULL   (lpThreadAttributes) */
-    0xFF, 0x53, offsetof (TrickContext, create_thread_impl),          /* call */
+    0x48, 0xC7, 0x44, 0x24, 0x28, 0x00, 0x00, 0x00, 0x00,                         /* mov qword [rsp + 0x28], 0 */
+    0xC7, 0x44, 0x24, 0x20, 0x00, 0x00, 0x00, 0x00,                               /* mov dword [rsp + 0x20], 0 */
+    0x49, 0x89, 0xC1,                                                             /* mov r9, rax               */
+    0x49, 0x89, 0xC0,                                                             /* mov r8, rax               */
+    0x4C, 0x03, 0x43, offsetof (TrickContext, worker_ctx_size),                   /* add r8, [rbx + 0xAA]      */
+    0x31, 0xD2,                                                                   /* xor edx, edx              */
+    0x48, 0x31, 0xC9,                                                             /* xor rcx, rcx              */
+    0xFF, 0x53, offsetof (TrickContext, create_thread_impl),                      /* call qword [rbx + 0xAA]   */
 
     /*
      * CloseHandle (worker_thread);
      */
-    0x50,                                 /* push eax               (hObject) */
-    0xFF, 0x53, offsetof (TrickContext, close_handle_impl),           /* call */
+    0x48, 0x89, 0xC1,                                                             /* mov rcx, rax            */
+    0xFF, 0x53, offsetof (TrickContext, close_handle_impl),                       /* call qword [rbx + 0xAA] */
 
     /*
      * Restore registers and continue execution from where we cut it off.
      */
-    0x8B, 0x43, offsetof (TrickContext, saved_xax),             /* mov eax, S */
-    0x8B, 0x4B, offsetof (TrickContext, saved_xcx),             /* mov ecx, S */
-    0x8B, 0x53, offsetof (TrickContext, saved_xdx),             /* mov edx, S */
-    0x8B, 0x7B, offsetof (TrickContext, saved_xdi),             /* mov edi, S */
-    0x8B, 0x73, offsetof (TrickContext, saved_xsi),             /* mov esi, S */
-    0x8B, 0x6B, offsetof (TrickContext, saved_xbp),             /* mov ebp, S */
-    0x8B, 0x63, offsetof (TrickContext, saved_xsp),             /* mov esp, S */
-    0xFF, 0x73, offsetof (TrickContext, saved_flags),           /* push ... S */
-    0xFF, 0x73, offsetof (TrickContext, saved_xip),             /* push ... S */
-    0x8B, 0x5B, offsetof (TrickContext, saved_xbx),             /* mov ebx, S */
-    0x83, 0xC4, 0x04,                                           /* add esp, 4 */
-    0x9D,                                                       /* popfd      */
-    0xFF, 0x64, 0x24, 0xF8                                      /* jmp [esp-8]*/
+    0x48, 0x8B, 0x83, offsetof (TrickContext, saved_xax),   0, 0, 0,              /* mov rax, S   */
+    0x48, 0x8B, 0x8B, offsetof (TrickContext, saved_xcx),   0, 0, 0,              /* mov rcx, S   */
+    0x48, 0x8B, 0x93, offsetof (TrickContext, saved_xdx),   0, 0, 0,              /* mov rdx, S   */
+    0x48, 0x8B, 0xBB, offsetof (TrickContext, saved_xdi),   0, 0, 0,              /* mov rdi, S   */
+    0x48, 0x8B, 0xB3, offsetof (TrickContext, saved_xsi),   0, 0, 0,              /* mov rsi, S   */
+    0x48, 0x8B, 0xAB, offsetof (TrickContext, saved_xbp),   0, 0, 0,              /* mov rbp, S   */
+    0x48, 0x8B, 0xA3, offsetof (TrickContext, saved_xsp),   0, 0, 0,              /* mov rsp, S   */
+    0x4C, 0x8B, 0x83, offsetof (TrickContext, saved_r8),    0, 0, 0,              /* mov  r8, S   */
+    0x4C, 0x8B, 0x8B, offsetof (TrickContext, saved_r9),    0, 0, 0,              /* mov  r9, S   */
+    0x4C, 0x8B, 0x93, offsetof (TrickContext, saved_r10),   0, 0, 0,              /* mov r10, S   */
+    0x4C, 0x8B, 0x9B, offsetof (TrickContext, saved_r11),   0, 0, 0,              /* mov r11, S   */
+    0x4C, 0x8B, 0xA3, offsetof (TrickContext, saved_r12),   0, 0, 0,              /* mov r12, S   */
+    0x4C, 0x8B, 0xAB, offsetof (TrickContext, saved_r13),   0, 0, 0,              /* mov r13, S   */
+    0x4C, 0x8B, 0xB3, offsetof (TrickContext, saved_r14),   0, 0, 0,              /* mov r14, S   */
+    0x4C, 0x8B, 0xBB, offsetof (TrickContext, saved_r15),   0, 0, 0,              /* mov r15, S   */
+    0xFF, 0xB3,       offsetof (TrickContext, saved_flags), 0, 0, 0,              /* push ... S   */
+    0xFF, 0xB3,       offsetof (TrickContext, saved_xip),   0, 0, 0,              /* push ... S   */
+    0x48, 0x8B, 0x9B, offsetof (TrickContext, saved_xbx),   0, 0, 0,              /* mov rbx, S   */
+    0x83, 0xC4, 0x08,                                                             /* add esp, 8   */
+    0x9D,                                                                         /* popfq        */
+    0x67, 0xFF, 0x64, 0x24, 0xF0,                                                 /* jmp [esp-16] */
+#else
+    /*
+     * guint8 * worker_data = VirtualAlloc (...);
+     */
+    0xFF, 0x73, offsetof (TrickContext, virtual_alloc_arg_protect),               /* push [ebx + 0xAA] */
+    0xFF, 0x73, offsetof (TrickContext, virtual_alloc_arg_allocation_type),       /* push [ebx + 0xAA] */
+    0xFF, 0x73, offsetof (TrickContext, virtual_alloc_arg_size),                  /* push [ebx + 0xAA] */
+    0xFF, 0x73, offsetof (TrickContext, virtual_alloc_arg_address),               /* push [ebx + 0xAA] */
+    0xFF, 0x53, offsetof (TrickContext, virtual_alloc_impl),                      /* call [ebx + 0xAA] */
+
+    /*
+     * memcpy (worker_data, ctx->worker_data, sizeof (ctx->worker_data));
+     */
+    0x89, 0xC7,                                                                   /* mov edi, eax          */
+    0x8B, 0x73, offsetof (TrickContext, memcpy_source),                           /* mov esi, [ebx + 0xAA] */
+    0x8B, 0x4B, offsetof (TrickContext, memcpy_size),                             /* mov ecx, [ebx + 0xAA] */
+    0xFC,                                                                         /* cld                   */
+    0xF3, 0xA5,                                                                   /* rep movsd             */
+
+    /*
+     * HANDLE worker_thread = CreateThread (...);
+     */
+    0x6A, 0x00,                                                                   /* push NULL           (lpThreadId) */
+    0x6A, 0x00,                                                                   /* push 0         (dwCreationFlags) */
+    0x50,                                                                         /* push eax           (lpParameter) */
+    0x03, 0x43, offsetof (TrickContext, worker_ctx_size),                         /* add eax, [ebx + 0xAA]            */
+    0x50,                                                                         /* push eax        (lpStartAddress) */
+    0x6A, 0x00,                                                                   /* push 0             (dwStackSize) */
+    0x6A, 0x00,                                                                   /* push NULL   (lpThreadAttributes) */
+    0xFF, 0x53, offsetof (TrickContext, create_thread_impl),                      /* call                             */
+
+    /*
+     * CloseHandle (worker_thread);
+     */
+    0x50,                                                                         /* push eax               (hObject) */
+    0xFF, 0x53, offsetof (TrickContext, close_handle_impl),                       /* call                             */
+
+    /*
+     * Restore registers and continue execution from where we cut it off.
+     */
+    0x8B, 0x43, offsetof (TrickContext, saved_xax),                               /* mov eax, S */
+    0x8B, 0x4B, offsetof (TrickContext, saved_xcx),                               /* mov ecx, S */
+    0x8B, 0x53, offsetof (TrickContext, saved_xdx),                               /* mov edx, S */
+    0x8B, 0x7B, offsetof (TrickContext, saved_xdi),                               /* mov edi, S */
+    0x8B, 0x73, offsetof (TrickContext, saved_xsi),                               /* mov esi, S */
+    0x8B, 0x6B, offsetof (TrickContext, saved_xbp),                               /* mov ebp, S */
+    0x8B, 0x63, offsetof (TrickContext, saved_xsp),                               /* mov esp, S */
+    0xFF, 0x73, offsetof (TrickContext, saved_flags),                             /* push ... S */
+    0xFF, 0x73, offsetof (TrickContext, saved_xip),                               /* push ... S */
+    0x8B, 0x5B, offsetof (TrickContext, saved_xbx),                               /* mov ebx, S */
+    0x83, 0xC4, 0x04,                                                             /* add esp, 4 */
+    0x9D,                                                                         /* popfd      */
+    0xFF, 0x64, 0x24, 0xF8                                                        /* jmp [esp-8]*/
+#endif
   };
   TrickContext trick_ctx;
   const guint trick_size = sizeof (trick_code) + sizeof (trick_ctx);
   guint8 worker_code[] = {
+#ifdef _M_X64
     /*
-     * Remove argument and return address, so VirtualFree will return directly
-     * to our caller instead of freed memory.
+     * Remove return address, so VirtualFree will return directly to
+     * ExitThread instead of freed memory.
      */
-    0x5E,                         /* pop esi                 (return address) */
-    0x5B,                         /* pop ebx         (WorkerContext argument) */
+    0x5E,                                             /* pop rsi              (return address) */
+    0x48, 0x89, 0xCB,                                 /* mov rbx, rcx (WorkerContext argument) */
+
+    /*
+     * Reserve stack space for arguments (24), rounded up to next 16 byte boundary (32)
+     */
+    0x48, 0x83, 0xEC, 32,                             /* sub rsp, 32                           */
 
     /*
      * LoadLibraryW (ctx->dll_path);
      */
-    0x8D, 0x43, offsetof (WorkerContext, dll_path),         /* lea eax, ebx+X */
-    0x50,                                                   /* push eax       */
-    0xFF, 0x53, offsetof (WorkerContext, load_library_impl),          /* call */
+    0x48, 0x8D, 0x4B, offsetof (WorkerContext, dll_path),           /* lea rcx, [rbx + 0xAA]   */
+    0xFF, 0x53,       offsetof (WorkerContext, load_library_impl),  /* call qword [rbx + 0xAA] */
+
+    /*
+     * VirtualFree (worker_data, ...); -> ExitThread ();
+     */
+    0x41, 0xB8, 0x00, 0x80, 0x00, 0x00,       /* mov r8d, MEM_RELEASE        (arg3: dwFreeType) */
+    0x48, 0x31, 0xD2,                         /* xor rdx, rdx                    (arg2: dwSize) */
+    0x48, 0x89, 0xD9,                         /* mov rcx, rbx     (VirtualFree arg1: lpAddress) */
+    0xFF, 0x73, offsetof (WorkerContext, exit_thread_impl),       /* push (fake return address) */
+    0xFF, 0x63, offsetof (WorkerContext, virtual_free_impl),                             /* jmp */
+#else
+    /*
+     * Remove argument and return address, so VirtualFree will return directly
+     * to our caller instead of freed memory.
+     */
+    0x5E,                                           /* pop esi                 (return address) */
+    0x5B,                                           /* pop ebx         (WorkerContext argument) */
+
+    /*
+     * LoadLibraryW (ctx->dll_path);
+     */
+    0x8D, 0x43, offsetof (WorkerContext, dll_path),                           /* lea eax, ebx+X */
+    0x50,                                                                     /* push eax       */
+    0xFF, 0x53, offsetof (WorkerContext, load_library_impl),                  /* call           */
 
     /*
      * VirtualFree (worker_data, ...);
      */
-    0x68, 0x00, 0x80, 0x00, 0x00, /* push MEM_RELEASE      (arg3: dwFreeType) */
-    0x6A, 0x00,                   /* push 0                    (arg2: dwSize) */
-    0x53,                         /* push ebx   (VirtualFree arg1: lpAddress) */
-    0x56,                         /* push esi           (fake return address) */
-    0xFF, 0x63, offsetof (WorkerContext, virtual_free_impl),           /* jmp */
-
-    0xCC, 0xCC, 0xCC              /* pad to multiple of 4                     */
+    0x68, 0x00, 0x80, 0x00, 0x00,                   /* push MEM_RELEASE      (arg3: dwFreeType) */
+    0x6A, 0x00,                                     /* push 0                    (arg2: dwSize) */
+    0x53,                                           /* push ebx   (VirtualFree arg1: lpAddress) */
+    0x56,                                           /* push esi           (fake return address) */
+    0xFF, 0x63, offsetof (WorkerContext, virtual_free_impl),                             /* jmp */
+#endif
   };
   WorkerContext worker_ctx;
   const guint worker_size = sizeof (worker_code) + sizeof (worker_ctx);
@@ -327,14 +439,18 @@ trick_thread_into_loading_dll (HANDLE process_handle, HANDLE thread_handle,
 
   kmod = GetModuleHandleW (L"kernel32.dll");
 
-  *((guint32 *) (trick_code +  8)) = worker_size;
-  *((guint32 *) (trick_code + 21)) = trick_size;
-  g_assert (worker_size % 4 == 0);
-  *((guint32 *) (trick_code + 26)) = worker_size / 4;
-  *((guint32 *) (trick_code + 39)) = sizeof (worker_ctx);
-
   trick_ctx.virtual_alloc_impl = GetProcAddress (kmod, "VirtualAlloc");
+  trick_ctx.virtual_alloc_arg_address = NULL;
+  trick_ctx.virtual_alloc_arg_size = worker_size;
+  trick_ctx.virtual_alloc_arg_allocation_type = MEM_COMMIT | MEM_RESERVE;
+  trick_ctx.virtual_alloc_arg_protect = PAGE_EXECUTE_READWRITE;
+
+  trick_ctx.memcpy_size = worker_size;
+
+  trick_ctx.worker_ctx_size = sizeof (worker_ctx);
+
   trick_ctx.create_thread_impl = GetProcAddress (kmod, "CreateThread");
+
   trick_ctx.close_handle_impl = GetProcAddress (kmod, "CloseHandle");
 
   ctx.ContextFlags = CONTEXT_INTEGER | CONTEXT_CONTROL;
@@ -372,12 +488,14 @@ trick_thread_into_loading_dll (HANDLE process_handle, HANDLE thread_handle,
   trick_ctx.saved_xsp = ctx.Esp;
   trick_ctx.saved_xsi = ctx.Esi;
   trick_ctx.saved_xdi = ctx.Edi;
+
   trick_ctx.saved_flags = ctx.EFlags;
   trick_ctx.saved_xip = ctx.Eip;
 #endif
 
   worker_ctx.load_library_impl = GetProcAddress (kmod, "LoadLibraryW");
   worker_ctx.virtual_free_impl = GetProcAddress (kmod, "VirtualFree");
+  worker_ctx.exit_thread_impl = GetProcAddress (kmod, "ExitThread");
 
   StringCbCopyW (worker_ctx.dll_path, sizeof (worker_ctx.dll_path), dll_path);
 
@@ -393,6 +511,8 @@ trick_thread_into_loading_dll (HANDLE process_handle, HANDLE thread_handle,
 
   remote_worker_ctx =
       (WorkerContext *) (((guint8 *) remote_trick_ctx) + trick_size);
+
+  trick_ctx.memcpy_source = remote_worker_ctx;
 
   if (!WriteProcessMemory (process_handle, remote_trick_ctx, &trick_ctx,
       sizeof (trick_ctx), NULL) ||
