fac344f92d4681fd57ea6ffcc4a26051e55e069d
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2010-05-17 22:18:08 +0200

    Improve error-reporting in case of unsupported signature. Move to Gum later.

--- lib/winagent/gst-tracer-glue.c
+++ lib/winagent/gst-tracer-glue.c
@@ -3,6 +3,9 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <tchar.h>
+#ifndef _M_X64
+#include <udis86.h>
+#endif
 
 typedef gchar * (* ZedGstObjectGetPathStringFunc) (gpointer object);
 typedef void (* ZedGstHeapFreeFunc) (gpointer mem);
@@ -53,13 +56,58 @@ zed_gst_tracer_attach (ZedGstTracer * self)
   switch (attach_ret)
   {
     case GUM_ATTACH_OK:
-      MessageBoxA (NULL, "Attached successfully!", "Yay", MB_ICONINFORMATION | MB_OK);
+      MessageBoxA (NULL, "Attached successfully!", "Yay",
+          MB_ICONINFORMATION | MB_OK);
       break;
     case GUM_ATTACH_WRONG_SIGNATURE:
-      MessageBoxA (NULL, "Function signature of gst_pad_push not supported by Gum.", "Error", MB_ICONERROR | MB_OK);
+    {
+      GString * msg;
+      ud_t ud_obj;
+      guint count = 0;
+
+      msg = g_string_new ("Function signature of gst_pad_push is not "
+          "supported by Gum.\r\n\r\nSignature:");
+
+      ud_init (&ud_obj);
+      ud_set_mode (&ud_obj, 32);
+      ud_set_syntax (&ud_obj, UD_SYN_INTEL);
+      ud_set_input_buffer (&ud_obj, pad_push_func, 4096);
+
+      while (count < 5)
+      {
+        guint insn_size;
+        guint i;
+
+        g_string_append (msg, "\r\n");
+
+        insn_size = ud_disassemble (&ud_obj);
+        g_assert (insn_size != 0);
+
+        for (i = 0; i != insn_size; i++)
+        {
+          if (i != 0)
+            g_string_append_c (msg, ' ');
+          g_string_append_printf (msg, "%02x",
+              ((guint8 *) pad_push_func)[count + i]);
+        }
+
+        g_string_append_c (msg, '\t');
+        if (insn_size < 3)
+          g_string_append_c (msg, '\t');
+
+        g_string_append (msg, ud_insn_asm (&ud_obj));
+
+        count += insn_size;
+      }
+
+      MessageBoxA (NULL, msg->str, "Error", MB_ICONERROR | MB_OK);
+      g_string_free (msg, TRUE);
+
       break;
+    }
     case GUM_ATTACH_ALREADY_ATTACHED:
-      MessageBoxA (NULL, "Already attached to gst_pad_push.", "Error", MB_ICONERROR | MB_OK);
+      MessageBoxA (NULL, "Already attached to gst_pad_push.", "Error",
+          MB_ICONERROR | MB_OK);
       break;
   }
 
