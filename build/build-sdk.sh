6e6e135f4aefadbf987b83d1a6e1e15704debb26
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2010-08-24 15:28:50 +0200

    Build system WIP.

--- build/build-sdk.sh
+++ build/build-sdk.sh
@@ -1,20 +1,48 @@
 #!/bin/sh
 
+function build_module ()
+{
+  if [ ! -d "$1" ]; then
+    git clone "$2" || exit 1
+    cd "$1"
+    if [ -n "$3" ]; then
+      git checkout "$3" || exit 1
+    fi
+    ./autogen.sh || exit 1
+    ./configure || exit 1
+    make install || exit 1
+  fi
+}
+
+function apply_fixups ()
+{
+  for file in $(find "$FRIDA_PREFIX" -type f); do
+    if grep -q "$FRIDA_PREFIX" $file; then
+      if echo "$file" | grep -Eq "\.la$"; then
+        newname="$file.frida.in"
+        mv "$file" "$newname"
+        sed -i "" -e "s,$FRIDA_PREFIX,@FRIDA_SDKROOT@,g" "$newname"
+      elif echo "$file" | grep -Eq "\.pc$"; then
+        sed -i "" -e "s,$FRIDA_PREFIX,\${frida_sdk_prefix},g" "$file"
+      fi
+    fi
+  done
+}
+
 [ -z "${FRIDA_BUILD}" ] && exit 1
+
+mkdir -p "${FRIDA_SDKROOT}/share/aclocal"
 
 BUILDROOT="${FRIDA_BUILD}/tmp-${FRIDA_TARGET}"
 mkdir -p "$BUILDROOT" || exit 1
 pushd "$BUILDROOT" &>/dev/null || exit 1
 
-if [ ! -d glib ]; then
-  git clone git@gitorious.org:frida/glib.git || exit 1
-  cd glib
-  ./autogen.sh || exit 1
-  ./configure || exit 1
-  make install || exit 1
-fi
+build_module glib "git@gitorious.org:frida/glib.git"
+build_module libgee "git://git.gnome.org/libgee" "0.5.2"
 
 popd &>/dev/null
+
+apply_fixups
 
 echo "All done."
 
--- build/fixup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sed -i "" "s,/Users/oleavr/Code/frida-ire/zed/iphone/build/toolchain,@FRIDA_TOOLROOT@,g" "$1"
-mv "$1" "$1.in"
-
