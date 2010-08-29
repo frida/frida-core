c23daf4d3512c238a6bbd3afb3f1116ea3405dc2
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2010-08-29 23:40:46 +0200

    Initialize Gum.

--- ios/src/agent-glue.c
+++ ios/src/agent-glue.c
@@ -1,8 +1,10 @@
 #include <glib-object.h>
+#include <gum/gum.h>
 
 __attribute__ ((constructor)) static void
 on_load (void)
 {
   g_type_init ();
+  gum_init_with_features (GUM_FEATURE_ALL & ~GUM_FEATURE_SYMBOL_LOOKUP);
 }
 
