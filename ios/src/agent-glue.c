e617386f12624fb4278a05e6b69e387717af7d95
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2010-09-25 14:40:21 +0200

    Build the iOS agent on Windows, so it can be easily tested with tools available there. The longer term plan is to migrate to using the same agent on Windows as well.

--- ios/src/agent-glue.c
+++ ios/src/agent-glue.c
@@ -1,10 +1,36 @@
 #include <glib-object.h>
 #include <gum/gum.h>
 
-__attribute__ ((constructor)) static void
+#ifdef G_OS_WIN32
+
+#define VC_EXTRALEAN
+#include <windows.h>
+
+static void on_load (void);
+
+BOOL APIENTRY
+DllMain (HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+  (void) hModule;
+  (void) ul_reason_for_call;
+  (void) lpReserved;
+
+  if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+  {
+    on_load ();
+  }
+
+  return TRUE;
+}
+
+#endif
+
+#ifdef __GNUC__
+__attribute__ ((constructor))
+#endif
+static void
 on_load (void)
 {
   g_type_init ();
   gum_init_with_features (GUM_FEATURE_ALL & ~GUM_FEATURE_SYMBOL_LOOKUP);
 }
-
