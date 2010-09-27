80aa5a18b9ace5a996e36376c432cd8bf929de9e
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2010-09-27 03:45:06 +0200

    Update SDK build script.

--- build/build-sdk.sh
+++ build/build-sdk.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
 
+function build_all ()
+{
+  mkdir -p "${FRIDA_SDKROOT}/share/aclocal"
+
+  BUILDROOT="${FRIDA_BUILD}/tmp-${FRIDA_TARGET}"
+  mkdir -p "$BUILDROOT" || exit 1
+  pushd "$BUILDROOT" &>/dev/null || exit 1
+
+  build_module glib "git@gitorious.org:frida/glib.git"
+  build_module libgee "git://git.gnome.org/libgee" "235d9b0410b68062911c414badf9a94bce1d34b6"
+
+  popd &>/dev/null
+}
+
 function build_module ()
 {
   if [ ! -d "$1" ]; then
     git clone "$2" || exit 1
-    cd "$1"
+    pushd "$1" &>/dev/null || exit 1
     if [ -n "$3" ]; then
       git checkout "$3" || exit 1
     fi
     ./autogen.sh || exit 1
     ./configure || exit 1
     make install || exit 1
+    popd &>/dev/null
   fi
 }
 
@@ -31,17 +46,7 @@ function apply_fixups ()
 
 [ -z "${FRIDA_BUILD}" ] && exit 1
 
-mkdir -p "${FRIDA_SDKROOT}/share/aclocal"
-
-BUILDROOT="${FRIDA_BUILD}/tmp-${FRIDA_TARGET}"
-mkdir -p "$BUILDROOT" || exit 1
-pushd "$BUILDROOT" &>/dev/null || exit 1
-
-build_module glib "git@gitorious.org:frida/glib.git"
-build_module libgee "git://git.gnome.org/libgee" "0.5.2"
-
-popd &>/dev/null
-
+build_all
 apply_fixups
 
 echo "All done."
