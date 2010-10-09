ac090d06cd1aa02be8618ea5c73beb75257c30c6
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2010-10-09 16:47:20 +0200

    Fix build-ios script.

--- build/build-ios.sh
+++ build/build-ios.sh
@@ -6,6 +6,6 @@ make install || exit 1
 popd >/dev/null
 
 pushd "${FRIDA_ROOT}" >/dev/null || exit 1
-./autogen.sh || exit 1
+./autogen.sh --disable-client --enable-server || exit 1
 make deploy || exit 1
 popd >/dev/null
