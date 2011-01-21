c4a302059748de241495ffe0f8100ad97c518df1
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2011-01-21 13:20:28 +0100

    Add support for entering a minimalistic environment for JHBuild purposes.

--- build/env.sh
+++ build/env.sh
@@ -2,7 +2,16 @@
 
 case $# in
   1)
-    export FRIDA_TARGET=$1
+    case $1 in
+      *-jhbuild)
+        export FRIDA_ENVIRONMENT=jhbuild
+        export FRIDA_TARGET=$(echo $1 | sed 's,-jhbuild$,,')
+        ;;
+      *)
+        export FRIDA_ENVIRONMENT=normal
+        export FRIDA_TARGET=$1
+        ;;
+    esac
     ;;
   *)
     echo "Format: $0 <TARGET>"
@@ -13,4 +22,3 @@ esac
 cd $(dirname $0)/..
 
 bash --rcfile build/bashrc
-
