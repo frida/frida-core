a46c83328238a1eea31328fb7bde3a2cb2769505
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2011-05-07 03:25:42 +0200

    Add stub Stopwatch implementation for POSIX systems.

---
+++ lib/agent/stopwatch-posix.c
@@ -0,0 +1,23 @@
+#include "zed-agent.h"
+
+void
+zed_agent_stopwatch_initialize (ZedAgentStopwatch * self)
+{
+}
+
+void
+zed_agent_stopwatch_restart (ZedAgentStopwatch * self)
+{
+}
+
+gdouble
+zed_agent_stopwatch_elapsed (ZedAgentStopwatch * self)
+{
+  return 0.0;
+}
+
+guint64
+zed_agent_stopwatch_elapsed_nanoseconds (ZedAgentStopwatch * self)
+{
+  return 0;
+}
