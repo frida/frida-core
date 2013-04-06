5ed246df113d3c29f9d219dc673d3afde1b3d675
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2013-04-06 17:51:52 +0200

    Move build and remove JHBuild support

---
+++ ext/build/env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+case $# in
+  1)
+    export FRIDA_TARGET=$1
+    ;;
+  *)
+    echo "Format: $0 <TARGET>"
+    exit 1
+    ;;
+esac
+
+cd $(dirname $0)/../../
+
+bash --rcfile ext/build/bashrc
