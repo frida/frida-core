dabd7f564ea626bced447bd7b09c54154f29f47a
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2013-04-13 20:20:01 +0200

    Fix iOS build regression

--- src/darwin/fruitjector-glue.c
+++ src/darwin/fruitjector-glue.c
@@ -1,6 +1,8 @@
 #include "frida-core.h"
 
-#include <crt_externs.h>
+#ifdef HAVE_MAC
+# include <crt_externs.h>
+#endif
 #include <errno.h>
 #include <spawn.h>
 #include <sys/stat.h>
@@ -8,11 +10,17 @@
 guint
 frida_fruitjector_helper_factory_spawn (const gchar * path, gchar ** argv, int argv_length, GError ** error)
 {
+  gchar ** envp;
   pid_t pid;
 
   chmod (path, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
 
-  if (posix_spawn (&pid, path, NULL, NULL, argv, *_NSGetEnviron ()) != 0)
+#ifdef HAVE_MAC
+  envp = *_NSGetEnviron ();
+#else
+  envp = NULL;
+#endif
+  if (posix_spawn (&pid, path, NULL, NULL, argv, envp) != 0)
     goto handle_spawn_error;
 
   return pid;
