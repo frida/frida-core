1279a1ad054cee99526407f46cea7ab91f6ee766
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2015-03-13 00:09:50 +0000

    Add labrats for linux-i386

--- tests/labrats/Makefile.linux.mk
+++ tests/labrats/Makefile.linux.mk
@@ -3,11 +3,23 @@ CFLAGS := -Wall -pipe -Os -fPIC -fdata-sections -ffunction-sections
 LDFLAGS := -Wl,--gc-sections
 
 all: \
+	unixvictim-linux-i386 \
 	unixvictim-linux-x86_64 \
+	unixattacker-linux-i386.so \
 	unixattacker-linux-x86_64.so
+
+unixvictim-linux-i386: unixvictim.c
+	$(CC) $(CFLAGS) $(LDFLAGS) -m32 $< -o $@.tmp
+	strip --strip-all $@.tmp
+	mv $@.tmp $@
 
 unixvictim-linux-x86_64: unixvictim.c
 	$(CC) $(CFLAGS) $(LDFLAGS) -m64 $< -o $@.tmp
+	strip --strip-all $@.tmp
+	mv $@.tmp $@
+
+unixattacker-linux-i386.so: unixattacker.c
+	$(CC) $(CFLAGS) $(LDFLAGS) -m32 -shared $< -o $@.tmp
 	strip --strip-all $@.tmp
 	mv $@.tmp $@
 
