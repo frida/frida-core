c77d936e8f29394d06a821ae23a05fbb8e000548
Ole André Vadla Ravnås <oleavr@gmail.com>
2015-05-20 13:11:33 +0200

    Add android-arm64 attacker/victim binaries

---
+++ tests/labrats/Makefile.android-arm64.mk
@@ -0,0 +1,52 @@
+CC := $$ANDROID_NDK_ROOT/toolchains/llvm-3.5/prebuilt/darwin-x86_64/bin/clang
+STRIP := $$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/darwin-x86_64/bin/aarch64-linux-android-strip
+CFLAGS := \
+	--sysroot=$$ANDROID_NDK_ROOT/platforms/android-21/arch-arm64 \
+	-gcc-toolchain $$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/darwin-x86_64 \
+	-target aarch64-none-linux-android \
+	-no-canonical-prefixes \
+	-Wall \
+	-pipe \
+	-fPIC \
+	-Os \
+	-fdata-sections -ffunction-sections \
+	-funwind-tables -fno-exceptions -fno-rtti \
+	-DANDROID \
+	-I$$ANDROID_NDK_ROOT/platforms/android-21/arch-arm64/usr/include
+LDFLAGS := \
+	-Wl,--no-undefined \
+	-Wl,-z,noexecstack \
+	-Wl,-z,relro \
+	-Wl,-z,now \
+	-Wl,--gc-sections
+
+all: \
+	unixvictim-android-arm64 \
+	unixattacker-android-arm64.so
+
+unixvictim-android-arm64: unixvictim.c
+	$(CC) $(CFLAGS) $(LDFLAGS) -fPIE $< -o $@.tmp
+	$(STRIP) --strip-all $@.tmp
+	mv $@.tmp $@
+
+unixattacker-android-arm64.so: unixattacker.c unixattacker-android-arm64.version
+	$(CC) $(CFLAGS) $(LDFLAGS) \
+		-shared \
+		-Wl,-soname,unixattacker-android-arm64.so \
+		-Wl,--version-script=unixattacker-android-arm64.version \
+		$< \
+		-o $@.tmp
+	$(STRIP) --strip-all $@.tmp
+	mv $@.tmp $@
+
+unixattacker-android-arm64.version:
+	echo "UNIXATTACKER_ANDROID_ARM64_1.0 {" > $@.tmp
+	echo "  global:"             >> $@.tmp
+	echo "    frida_agent_main;" >> $@.tmp
+	echo ""                      >> $@.tmp
+	echo "  local:"              >> $@.tmp
+	echo "    *;"                >> $@.tmp
+	echo "};"                    >> $@.tmp
+	mv $@.tmp $@
+
+.PHONY: all
