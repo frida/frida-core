503121dc771a093b71362199c6ad20fe0f9973a7
Ole André Vadla Ravnås <oleavr@gmail.com>
2016-12-20 21:02:52 +0100

    Revive tests on Android/i386

--- tests/labrats/Makefile.android-i386.mk
+++ tests/labrats/Makefile.android-i386.mk
@@ -1,13 +1,13 @@
-CC := $$ANDROID_NDK_ROOT/toolchains/llvm-3.4/prebuilt/darwin-x86_64/bin/clang
-STRIP := $$ANDROID_NDK_ROOT/toolchains/x86-4.8/prebuilt/darwin-x86_64/bin/i686-linux-android-strip
+CC := $$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/darwin-x86_64/bin/clang
+STRIP := $$ANDROID_NDK_ROOT/toolchains/x86-4.9/prebuilt/darwin-x86_64/bin/i686-linux-android-strip
 CFLAGS := \
 	--sysroot=$$ANDROID_NDK_ROOT/platforms/android-14/arch-x86 \
-	-gcc-toolchain $$ANDROID_NDK_ROOT/toolchains/x86-4.8/prebuilt/darwin-x86_64 \
+	-gcc-toolchain $$ANDROID_NDK_ROOT/toolchains/x86-4.9/prebuilt/darwin-x86_64 \
 	-target i686-none-linux-android \
 	-no-canonical-prefixes \
 	-Wall \
 	-pipe \
-	-fPIC \
+	-fPIC -fPIE \
 	-Os \
 	-march=i686 \
 	-fdata-sections -ffunction-sections \
@@ -23,31 +23,33 @@ LDFLAGS := \
 
 all: \
 	sleeper-android-i386 \
-	simple-agent-android-i386.so
+	simple-agent-android-i386.so \
+	resident-agent-android-i386.so \
+	$(NULL)
 
 sleeper-android-i386: sleeper-unix.c
-	$(CC) $(CFLAGS) $(LDFLAGS) $< -o $@.tmp
+	$(CC) $(CFLAGS) $(LDFLAGS) -pie $< -o $@.tmp
 	$(STRIP) --strip-all $@.tmp
 	mv $@.tmp $@
 
-simple-agent-android-i386.so: simple-agent.c simple-agent-android-i386.version
+%-agent-android-i386.so: %-agent.c %-agent-android-i386.version
 	$(CC) $(CFLAGS) $(LDFLAGS) \
 		-shared \
-		-Wl,-soname,simple-agent-android-i386.so \
-		-Wl,--version-script=simple-agent-android-i386.version \
+		-Wl,-soname,$*-agent-android-i386.so \
+		-Wl,--version-script=$*-agent-android-i386.version \
 		$< \
 		-o $@.tmp
 	$(STRIP) --strip-all $@.tmp
 	mv $@.tmp $@
 
-simple-agent-android-i386.version:
-	echo "SIMPLE_AGENT_ANDROID_I386_1.0 {"     > $@.tmp
-	echo "  global:"             >> $@.tmp
-	echo "    frida_agent_main;" >> $@.tmp
-	echo ""                      >> $@.tmp
-	echo "  local:"              >> $@.tmp
-	echo "    *;"                >> $@.tmp
-	echo "};"                    >> $@.tmp
+%-agent-android-i386.version:
+	echo "LABRAT_AGENT_ANDROID_I386_1.0 {"  > $@.tmp
+	echo "  global:"                       >> $@.tmp
+	echo "    frida_agent_main;"           >> $@.tmp
+	echo ""                                >> $@.tmp
+	echo "  local:"                        >> $@.tmp
+	echo "    *;"                          >> $@.tmp
+	echo "};"                              >> $@.tmp
 	mv $@.tmp $@
 
 .PHONY: all
