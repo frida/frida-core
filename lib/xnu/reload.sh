8b11057353ad08a7f0a8b2bb6db58e742c30b935
Viktor Oreshkin <solteke.st@gmail.com>
2017-10-11 20:30:27 +0300

    Improve XNU driver reload.sh and README (#150)

--- lib/xnu/reload.sh
+++ lib/xnu/reload.sh
@@ -1,7 +1,33 @@
 #!/bin/sh
-sudo kextunload /Library/Extensions/FridaKernelAgent.kext &>/dev/null
-sudo rm -rf /Library/Extensions/FridaKernelAgent.kext
-sudo cp -a ~/Library/Developer/Xcode/DerivedData/FridaKernelAgent-eaueaeplpgvooydjbumkfbqjrobj/Build/Products/Debug/FridaKernelAgent.kext /Library/Extensions/FridaKernelAgent.kext
-sudo chown -R root /Library/Extensions/FridaKernelAgent.kext
-sudo chgrp -R wheel /Library/Extensions/FridaKernelAgent.kext
-sudo kextload /Library/Extensions/FridaKernelAgent.kext
+
+KEXT_NAME=FridaKernelAgent.kext
+
+# Unload and remove old kext (does nothing on first build)
+sudo kextunload "/Library/Extensions/$KEXT_NAME" &>/dev/null
+sudo rm -rf "/Library/Extensions/$KEXT_NAME"
+
+# Set up temp dir and cleanup trap
+BUILD_DIR="$(mktemp -d)"
+
+cleanup () {
+  rm -rf "$BUILD_DIR"
+}
+trap cleanup EXIT
+
+# cd to sources
+cd "$(dirname "$(realpath "$0")")"
+
+xcodebuild build \
+  CONFIGURATION_BUILD_DIR="$BUILD_DIR" \
+  CODE_SIGN_IDENTITY="$CODE_SIGN_IDENTITY" \
+  &>/dev/null
+
+if [ $? -ne 0 ]; then
+  echo "Failed to build kext" >&2
+  exit 1
+fi
+
+# Move, fix permissions, and load
+sudo mv "$BUILD_DIR/$KEXT_NAME" "/Library/Extensions/$KEXT_NAME"
+sudo chown -R root:wheel "/Library/Extensions/$KEXT_NAME"
+sudo kextload "/Library/Extensions/$KEXT_NAME"
