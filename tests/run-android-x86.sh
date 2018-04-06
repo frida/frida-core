06cc5cc1a6bf0ab857e6848159500f150b17a73b
Ole André Vadla Ravnås <oleavr@gmail.com>
2018-04-06 02:27:58 +0200

    Add runner scripts for Android

---
+++ tests/run-android-x86.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+arch=x86
+
+remote_prefix=/data/local/tmp/frida-tests-$arch
+
+core_tests=$(dirname "$0")
+cd "$core_tests/../../build/tmp-android-$arch/frida-core" || exit 1
+. ../../frida-meson-env-macos-x86_64.rc
+ninja || exit 1
+cd tests
+adb shell "mkdir $remote_prefix"
+adb push frida-tests labrats ../lib/agent/frida-agent.so $remote_prefix || exit 1
+adb shell "$remote_prefix/frida-tests $@"
