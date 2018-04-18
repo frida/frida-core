0e3c6b8050226ac89b75d510f1aa2745d06f9f13
Ole André Vadla Ravnås <oleavr@gmail.com>
2018-04-18 21:09:09 +0200

    Fix stylistic inconsistency

--- tests/labrats/resident-agent.c
+++ tests/labrats/resident-agent.c
@@ -48,8 +48,7 @@ on_unload (void)
 #endif
 
 void
-frida_agent_main (const char * data,
-                  bool * stay_resident)
+frida_agent_main (const char * data, bool * stay_resident)
 {
   (void) data;
 
