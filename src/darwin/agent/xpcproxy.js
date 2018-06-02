99a9837f9a4455b8e28879ba08fc576bd287914a
Ole André Vadla Ravnås <oleavr@gmail.com>
2018-06-02 02:29:16 +0200

    Fix compatibility with Electra's Tweak Injector

--- src/darwin/agent/xpcproxy.js
+++ src/darwin/agent/xpcproxy.js
@@ -2,6 +2,9 @@
 
 var POSIX_SPAWN_START_SUSPENDED = 0x0080;
 
+var jbdCallImpl = Module.findExportByName(null, 'jbd_call');
+var runningOnElectra = jbdCallImpl !== null;
+
 Interceptor.attach(Module.findExportByName('/usr/lib/system/libsystem_kernel.dylib', '__posix_spawn'), {
   onEnter: function (args) {
     var attrs = Memory.readPointer(args[2].add(Process.pointerSize));
@@ -11,3 +14,18 @@ Interceptor.attach(Module.findExportByName('/usr/lib/system/libsystem_kernel.dyl
     Memory.writeU16(attrs, flags);
   }
 });
+
+if (runningOnElectra) {
+  sabotageJbdCall();
+}
+
+function sabotageJbdCall() {
+  var retType = 'int';
+  var argTypes = ['uint', 'uint', 'uint'];
+
+  var jbdCall = new NativeFunction(jbdCallImpl, retType, argTypes);
+
+  Interceptor.replace(jbdCall, new NativeCallback(function (port, command, pid) {
+    return 0;
+  }, retType, argTypes));
+}
