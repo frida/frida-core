bece0fbd2ea2511c7a74ab2124acfd1e73138693
Ole André Vadla Ravnås <oleavr@gmail.com>
2019-01-14 21:35:56 +0100

    Always symbolicate and generate crash reports
    But delete the report afterwards if it wouldn't normally have been
    generated.

--- src/darwin/agent/reportcrash.js
+++ src/darwin/agent/reportcrash.js
@@ -1,16 +1,24 @@
 'use strict';
 
 var LIBSYSTEM_KERNEL_PATH = '/usr/lib/system/libsystem_kernel.dylib';
+var CRASH_REPORTER_SUPPORT_PATH = '/System/Library/PrivateFrameworks/CrashReporterSupport.framework/CrashReporterSupport';
 
 var pidForTask = new NativeFunction(
     Module.findExportByName(LIBSYSTEM_KERNEL_PATH, 'pid_for_task'),
     'int',
     ['uint', 'pointer']
 );
+var unlink = new NativeFunction(
+    Module.findExportByName(LIBSYSTEM_KERNEL_PATH, 'unlink'),
+    'int',
+    ['pointer']
+);
 
 var pid = -1;
+var logPath = null;
 var logFd = null;
 var logChunks = [];
+var forcedByUs = false;
 
 var CrashReport = ObjC.classes.CrashReport;
 if (CrashReport !== undefined) {
@@ -68,5 +76,59 @@ Interceptor.attach(Module.findExportByName(LIBSYSTEM_KERNEL_PATH, 'write'), {
       return;
     var chunk = Memory.readUtf8String(this.buf, n);
     logChunks.push(chunk);
+  }
+});
+
+Interceptor.attach(ObjC.classes.CrashReport['- isActionable'].implementation, {
+  onLeave: function (retval) {
+    var isActionable = !!retval.toInt32();
+    if (!isActionable) {
+      retval.replace(ptr(1));
+      forcedByUs = true;
+    }
+  }
+});
+
+Interceptor.attach(ObjC.classes.NSMutableDictionary['- logCounter_isLog:byKey:count:withinLimit:withOptions:'].implementation, {
+  onLeave: function (retval) {
+    var isLogWithinLimit = !!retval.toInt32();
+    if (!isLogWithinLimit) {
+      retval.replace(ptr(1));
+      forcedByUs = true;
+    }
+  },
+});
+
+Interceptor.attach(ObjC.classes.AppleErrorReport['- saveToDir:'].implementation, {
+  onLeave: function (retval) {
+    if (forcedByUs) {
+      unlink(Memory.allocUtf8String(logPath));
+      logPath = null;
+      forcedByUs = false;
+    }
+  }
+});
+
+Interceptor.attach(Module.findExportByName(LIBSYSTEM_KERNEL_PATH, 'rename'), {
+  onEnter: function (args) {
+    var newPath = Memory.readUtf8String(args[1]);
+    if (/\.ips$/.test(newPath)) {
+      logPath = newPath;
+    }
+  },
+});
+
+Interceptor.attach(Module.findExportByName(CRASH_REPORTER_SUPPORT_PATH, 'OSAPreferencesGetBoolValue'), {
+  onEnter: function (args) {
+    this.name = new ObjC.Object(args[0]).toString();
+    this.domain = new ObjC.Object(args[1]).toString();
+    this.successPtr = args[2];
+  },
+  onLeave: function (retval) {
+    if (this.name === 'SymbolicateCrashes' && this.domain === 'com.apple.CrashReporter') {
+      if (!this.successPtr.isNull())
+        Memory.writeU8(this.successPtr, 1);
+      retval.replace(ptr(1));
+    }
   }
 });
