c332d73f9a67c6352213eef349ff4624d2a9239b
Ole André Vadla Ravnås <oleavr@gmail.com>
2019-03-14 21:45:03 +0100

    Fix spawn() on substrated-based iOS jailbreaks

--- src/darwin/agent/launchd.js
+++ src/darwin/agent/launchd.js
@@ -17,6 +17,8 @@ var jbdCallImpl = Module.findExportByName(null, 'jbd_call');
 var jbdPidsToIgnore = {};
 var runningOnElectra = jbdCallImpl !== null;
 
+var substratePidsPending = {};
+
 rpc.exports = {
   prepareForLaunch: function (identifier) {
     upcoming[identifier] = true;
@@ -82,10 +84,19 @@ Interceptor.attach(Module.findExportByName('/usr/lib/system/libsystem_kernel.dyl
 
     var pid = readU32(this.pidPtr);
 
-    send([event, identifier, pid]);
-
     if (runningOnElectra) {
       jbdPidsToIgnore[pid] = true;
+    }
+
+    var dealingWithSubstrate = this.depth > 0;
+    if (dealingWithSubstrate) {
+      substratePidsPending[pid] = notifyFridaBackend;
+    } else {
+      notifyFridaBackend();
+    }
+
+    function notifyFridaBackend() {
+      send([event, identifier, pid]);
     }
   }
 });
@@ -109,4 +120,70 @@ function sabotageJbdCallForOurPids() {
 
     return jbdCall(port, command, pid);
   }, retType, argTypes));
+}
+
+var launcher = tryDetectSubstrateLauncher();
+if (launcher !== null) {
+  instrumentSubstrateLauncher(launcher);
+}
+
+function tryDetectSubstrateLauncher() {
+  if (Process.arch !== 'arm64')
+    return null;
+
+  var LAUNCHER_T_DYLIB_NAME = '4c 61 75 6e 63 68 65 72 2e 74 2e 64 79 6c 69 62';
+
+  var modules = new ModuleMap();
+  var ranges = Process.enumerateRanges('r-x')
+      .filter(function (r) { return !modules.has(r.base); })
+      .filter(function (r) { return (r.base.readU32() & 0xfffffffe) >>> 0 === 0xfeedface; })
+      .filter(function (r) { return Memory.scanSync(r.base, 2048, LAUNCHER_T_DYLIB_NAME).length > 0; });
+  if (ranges.length === 0)
+    return null;
+  var launcher = ranges[0];
+  var base = launcher.base;
+  var size = launcher.size;
+
+  return {
+    handlePosixSpawn: resolveFunction('fd 7b bf a9 fd 03 00 91 f4 4f bf a9 f6 57 bf a9 f8 5f bf a9 fa 67 bf a9 fc 6f bf a9 ff 03 04 d1'),
+    workerCont: resolveFunction('fd 7b bf a9 fd 03 00 91 f4 4f bf a9 f6 57 bf a9 ff 83 01 d1'),
+  };
+
+  function resolveFunction(signature) {
+    var matches = Memory.scanSync(base, size, signature);
+    if (matches.length !== 1) {
+      throw new Error('Unsupported version of Substrate; please file a bug');
+    }
+    return matches[0].address;
+  }
+}
+
+function instrumentSubstrateLauncher(launcher) {
+  Interceptor.attach(launcher.handlePosixSpawn, {
+    onEnter: function () {
+      // Empty because our __posix_spawn() hook is informed by the InvocationContext depth.
+    }
+  });
+
+  Interceptor.attach(launcher.workerCont, {
+    onEnter: function (args) {
+      var baton = args[0];
+      var pid = baton.readS32();
+
+      var notify = substratePidsPending[pid];
+      if (notify !== undefined) {
+        delete substratePidsPending[pid];
+
+        var startSuspendedPtr = baton.add(4);
+        startSuspendedPtr.writeU8(1);
+
+        this.notify = notify;
+      }
+    },
+    onLeave: function (retval) {
+      var notify = this.notify;
+      if (notify !== undefined)
+        notify();
+    },
+  });
 }
--- src/darwin/agent/xpcproxy.js
+++ src/darwin/agent/xpcproxy.js
@@ -29,3 +29,47 @@ function sabotageJbdCall() {
     return 0;
   }, retType, argTypes));
 }
+
+var proxyer = tryDetectSubstrateProxyer();
+if (proxyer !== null) {
+  instrumentSubstrateProxyer(proxyer);
+}
+
+function tryDetectSubstrateProxyer() {
+  if (Process.arch !== 'arm64')
+    return null;
+
+  var PROXYER_T_DYLIB_NAME = '50 72 6f 78 79 65 72 2e 74 2e 64 79 6c 69 62';
+
+  var modules = new ModuleMap();
+  var ranges = Process.enumerateRanges('r-x')
+      .filter(function (r) { return !modules.has(r.base); })
+      .filter(function (r) { return (r.base.readU32() & 0xfffffffe) >>> 0 === 0xfeedface; })
+      .filter(function (r) { return Memory.scanSync(r.base, 2048, PROXYER_T_DYLIB_NAME).length > 0; });
+  if (ranges.length === 0)
+    return null;
+  var proxyer = ranges[0];
+  var base = proxyer.base;
+  var size = proxyer.size;
+
+  return {
+    exec: resolveFunction('fd 7b bf a9 fd 03 00 91 f4 4f bf a9 ff 03 01 d1 f3 03 03 aa'),
+  };
+
+  function resolveFunction(signature) {
+    var matches = Memory.scanSync(base, size, signature);
+    if (matches.length !== 1) {
+      throw new Error('Unsupported version of Substrate; please file a bug');
+    }
+    return matches[0].address;
+  }
+}
+
+function instrumentSubstrateProxyer(proxyer) {
+  Interceptor.attach(proxyer.exec, {
+    onEnter: function (args) {
+      var startSuspendedYup = ptr(1);
+      args[2] = startSuspendedYup;
+    }
+  });
+}
