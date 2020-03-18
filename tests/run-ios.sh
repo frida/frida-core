c62d265de3ff6ec9651e496d4fc66d6a3da353ba
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-03-18 19:10:54 +0100

    Tweak tests/run-ios.sh to do a full build

--- tests/run-ios.sh
+++ tests/run-ios.sh
@@ -5,6 +5,7 @@ arch=arm64e
 remote_host=iphone
 remote_prefix=/usr/local/opt/frida-tests-$arch
 
+make -C .. build/.core-ios-stamp-frida-ios-$arch
 core_tests=$(dirname "$0")
 cd "$core_tests/../../build/tmp-ios-$arch/frida-core" || exit 1
 . ../../frida-meson-env-macos-x86_64.rc
