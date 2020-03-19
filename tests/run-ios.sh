6e665709af00d660c1c5bfab7d961c369435f750
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-03-19 20:00:58 +0100

    Tweak tests/run-ios.sh to ensure remote prefix exists

--- tests/run-ios.sh
+++ tests/run-ios.sh
@@ -5,12 +5,18 @@ arch=arm64e
 remote_host=iphone
 remote_prefix=/usr/local/opt/frida-tests-$arch
 
-make -C .. build/.core-ios-stamp-frida-ios-$arch
 core_tests=$(cd $(dirname "$0") && pwd)
+
+make -C .. build/.core-ios-stamp-frida-ios-$arch
+
 cd "$core_tests/../../build/tmp-ios-$arch/frida-core" || exit 1
+
 . ../../frida-meson-env-macos-x86_64.rc
 ninja || exit 1
+
 cd tests
+
+ssh "$remote_host" "mkdir -p '$remote_prefix'"
 rsync -rLz \
   frida-tests \
   labrats \
@@ -18,4 +24,5 @@ rsync -rLz \
   ../../../frida-ios-arm64e/lib/frida-gadget.dylib \
   "$core_tests/test-gadget-standalone.js" \
   "$remote_host:$remote_prefix/" || exit 1
+
 ssh "$remote_host" "$remote_prefix/frida-tests" "$@"
