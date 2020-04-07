02cce0893574ed7a36ac8536313ac5ca62feab91
Zmurf <crax0r@gmail.com>
2020-04-07 17:58:36 +0200

    Add support for leanback-only Android apps (#299)

--- src/linux/agent/system-server.js
+++ src/linux/agent/system-server.js
@@ -100,6 +100,8 @@ rpc.exports = {
         throw new Error("Unable to find application with identifier '" + pkg + "'");
 
       var intent = packageManager.getLaunchIntentForPackage(pkg);
+      if (intent === null && 'getLeanbackLaunchIntentForPackage' in packageManager)
+        intent = packageManager.getLeanbackLaunchIntentForPackage(pkg);
       if (intent === null && activity === null)
         throw new Error('Unable to find a front-door activity');
 
