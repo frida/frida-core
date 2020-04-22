72a9eaecd443221a562dfcf0112f880cc6a04b86
Francesco Tamagni <mrmacete@protonmail.ch>
2020-04-23 00:06:45 +0200

    Add four essential i/macOS syscalls to the libc shim (#307)
    Covering:
    - read()
    - write()
    - mmap()
    - munmap()

--- lib/payload/libc-shim.c
+++ lib/payload/libc-shim.c
@@ -447,3 +447,204 @@ _frida_set_errno (int n)
 }
 
 #endif
+
+#if defined (HAVE_DARWIN) && GLIB_SIZEOF_VOID_P == 8
+
+# undef read
+# undef write
+# undef mmap
+# undef munmap
+
+ssize_t
+read (int fildes, const void * buf, size_t nbyte)
+{
+  ssize_t result;
+
+# ifdef HAVE_I386
+  register          gint rdi asm ("rdi") = fildes;
+  register gconstpointer rsi asm ("rsi") = buf;
+  register         gsize rdx asm ("rdx") = nbyte;
+  register         guint eax asm ("eax") = 0x2000003;
+
+  asm volatile (
+      "syscall\n\t"
+      : "=a" (result)
+      : "r" (rdi),
+        "r" (rsi),
+        "r" (rdx),
+        "r" (eax)
+      : "rcx", "r11", "cc", "memory"
+  );
+# else
+  asm volatile (
+      "sub sp, sp, #16 * 2\n\t"
+      "stp x0, x1, [sp, #16 * 0]\n\t"
+      "stp x2, x16, [sp, #16 * 1]\n\t"
+      "mov x0, %1\n\t"
+      "mov x1, %2\n\t"
+      "mov x2, %3\n\t"
+      "mov x16, 0x3\n\t"
+      "svc 0x80\n\t"
+      "mov %0, x0\n\t"
+      "ldp x0, x1, [sp, #16 * 0]\n\t"
+      "ldp x2, x16, [sp, #16 * 1]\n\t"
+      "add sp, sp, #16 * 2\n\t"
+      : "=r" (result)
+      : "r" ((gsize) fildes),
+        "r" (buf),
+        "r" (nbyte)
+      : "x0", "x1", "x2", "x16"
+  );
+# endif
+
+  return result;
+}
+
+ssize_t
+write (int fildes, const void * buf, size_t nbyte)
+{
+  ssize_t result;
+
+# ifdef HAVE_I386
+  register          gint rdi asm ("rdi") = fildes;
+  register gconstpointer rsi asm ("rsi") = buf;
+  register         gsize rdx asm ("rdx") = nbyte;
+  register         guint eax asm ("eax") = 0x2000004;
+
+  asm volatile (
+      "syscall\n\t"
+      : "=a" (result)
+      : "r" (rdi),
+        "r" (rsi),
+        "r" (rdx),
+        "r" (eax)
+      : "rcx", "r11", "cc", "memory"
+  );
+# else
+  asm volatile (
+      "sub sp, sp, #16 * 2\n\t"
+      "stp x0, x1, [sp, #16 * 0]\n\t"
+      "stp x2, x16, [sp, #16 * 1]\n\t"
+      "mov x0, %1\n\t"
+      "mov x1, %2\n\t"
+      "mov x2, %3\n\t"
+      "mov x16, 0x4\n\t"
+      "svc 0x80\n\t"
+      "mov %0, x0\n\t"
+      "ldp x0, x1, [sp, #16 * 0]\n\t"
+      "ldp x2, x16, [sp, #16 * 1]\n\t"
+      "add sp, sp, #16 * 2\n\t"
+      : "=r" (result)
+      : "r" ((gsize) fildes),
+        "r" (buf),
+        "r" (nbyte)
+      : "x0", "x1", "x2", "x16"
+  );
+# endif
+
+  return result;
+}
+
+void *
+mmap (void * addr, size_t len, int prot, int flags, int fd, off_t offset)
+{
+  void * result;
+
+# ifdef HAVE_I386
+  register      gpointer rdi asm ("rdi") = addr;
+  register         gsize rsi asm ("rsi") = len;
+  register         gsize rdx asm ("rdx") = (gsize) prot;
+  register         gsize r10 asm ("r10") = (gsize) flags;
+  register         gsize  r8 asm ( "r8") = (gsize) fd;
+  register         gsize  r9 asm ( "r9") = offset;
+  register         guint eax asm ("eax") = 0x20000c5;
+
+  asm volatile (
+      "syscall\n\t"
+      : "=a" (result)
+      : "r" (rdi),
+        "r" (rsi),
+        "r" (rdx),
+        "r" (r10),
+        "r" (r8),
+        "r" (r9),
+        "r" (eax)
+      : "rcx", "r11", "cc", "memory"
+  );
+# else
+  asm volatile (
+      "sub sp, sp, #16 * 4\n\t"
+      "stp x0, x1, [sp, #16 * 0]\n\t"
+      "stp x2, x3, [sp, #16 * 1]\n\t"
+      "stp x4, x5, [sp, #16 * 2]\n\t"
+      "str x16, [sp, #16 * 3]\n\t"
+      "mov x0, %1\n\t"
+      "mov x1, %2\n\t"
+      "mov x2, %3\n\t"
+      "mov x3, %4\n\t"
+      "mov x4, %5\n\t"
+      "mov x5, %6\n\t"
+      "mov x16, 0xc5\n\t"
+      "svc 0x80\n\t"
+      "mov %0, x0\n\t"
+      "ldp x0, x1, [sp, #16 * 0]\n\t"
+      "ldp x2, x3, [sp, #16 * 1]\n\t"
+      "ldp x4, x5, [sp, #16 * 2]\n\t"
+      "ldr x16, [sp, #16 * 3]\n\t"
+      "add sp, sp, #16 * 4\n\t"
+      : "=r" (result)
+      : "r" (addr),
+        "r" (len),
+        "r" ((gsize) prot),
+        "r" ((gsize) flags),
+        "r" ((gsize) fd),
+        "r" (offset)
+      : "x0", "x1", "x2", "x3", "x4", "x5", "x16"
+  );
+# endif
+
+  return result;
+}
+
+int
+munmap (void * addr, size_t len)
+{
+  int result;
+
+# ifdef HAVE_I386
+  register      gpointer rdi asm ("rdi") = addr;
+  register         gsize rsi asm ("rsi") = len;
+  register         guint eax asm ("eax") = 0x2000049;
+
+  asm volatile (
+      "syscall\n\t"
+      : "=a" (result)
+      : "r" (rdi),
+        "r" (rsi),
+        "r" (eax)
+      : "rcx", "r11", "cc", "memory"
+  );
+# else
+  asm volatile (
+      "sub sp, sp, #16 * 2\n\t"
+      "stp x0, x1, [sp, #16 * 0]\n\t"
+      "str x16, [sp, #16 * 1]\n\t"
+      "mov x0, %1\n\t"
+      "mov x1, %2\n\t"
+      "mov x16, 0x49\n\t"
+      "svc 0x80\n\t"
+      "mov %w0, w0\n\t"
+      "ldp x0, x1, [sp, #16 * 0]\n\t"
+      "ldr x16, [sp, #16 * 1]\n\t"
+      "add sp, sp, #16 * 2\n\t"
+      : "=r" (result)
+      : "r" (addr),
+        "r" (len)
+      : "x0", "x1", "x16"
+  );
+# endif
+
+  return result;
+}
+
+#endif
