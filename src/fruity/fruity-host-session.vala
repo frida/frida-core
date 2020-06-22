frida/frida-core

commit 95433bedb4365909ba22d48bdf01653824c16b50

Add beginnings of a native lockdown integration
This is considered unstable API.

src/fruity/fruity-host-session.vala
@@ -1,12 +1,14 @@
 namespace Frida {
 	public class FruityHostSessionBackend : Object, HostSessionBackend {
-		private Fruity.Client control_client;
-		private Gee.HashMap<uint, FruityHostSessionProvider> provider_by_device_id = new Gee.HashMap<uint, FruityHostSessionProvider> ();
+		private Fruity.UsbmuxClient control_client;
+
+		private Gee.HashSet<uint> devices = new Gee.HashSet<uint> ();
+		private Gee.HashMap<uint, FruityRemoteProvider> remote_providers = new Gee.HashMap<uint, FruityRemoteProvider> ();
+		private Gee.HashMap<uint, FruityLockdownProvider> lockdown_providers = new Gee.HashMap<uint, FruityLockdownProvider> ();
+
 		private Gee.Promise<bool> start_request;
 		private StartedHandler started_handler;
 		private delegate void StartedHandler ();
-		private bool has_probed_protocol_version = false;
-		private uint protocol_version = 1;
 
 		public async void start () {
 			started_handler = () => start.callback ();
@@ -27,39 +29,27 @@ namespace Frida {
 
 			bool success = true;
 
-			control_client = yield create_client ();
-			control_client.device_attached.connect ((id, product_id, udid) => {
-				if (provider_by_device_id.has_key (id))
-					return;
-
-				var provider = new FruityHostSessionProvider (this, id, product_id, udid);
-				provider_by_device_id[id] = provider;
-				open_provider.begin (provider);
-			});
-			control_client.device_detached.connect ((id) => {
-				if (!provider_by_device_id.has_key (id))
-					return;
-
-				FruityHostSessionProvider provider;
-				provider_by_device_id.unset (id, out provider);
-
-				if (provider.is_open)
-					provider_unavailable (provider);
-			});
-
 			try {
-				yield control_client.establish ();
+				control_client = yield Fruity.UsbmuxClient.open ();
+
+				control_client.device_attached.connect ((details) => {
+					add_device.begin (details);
+				});
+				control_client.device_detached.connect ((id) => {
+					remove_device (id);
+				});
+
 				yield control_client.enable_listen_mode ();
-			} catch (IOError e) {
+			} catch (Fruity.UsbmuxError e) {
 				success = false;
 			}
 
 			if (success) {
 				/* perform a dummy-request to flush out any pending device attach notifications */
 				try {
-					yield control_client.connect_to_port (uint.MAX, uint.MAX);
+					yield control_client.connect_to_port (Fruity.DeviceId (uint.MAX), 0);
 					assert_not_reached ();
-				} catch (IOError expected_error) {
+				} catch (Fruity.UsbmuxError expected_error) {
 				}
 			}
 
@@ -80,126 +70,45 @@ namespace Frida {
 			}
 
 			if (control_client != null) {
-				try {
-					yield control_client.close ();
-				} catch (IOError e) {
-				}
+				yield control_client.close ();
 				control_client = null;
 			}
 
-			foreach (var provider in provider_by_device_id.values) {
-				if (provider.is_open)
-					provider_unavailable (provider);
+			devices.clear ();
+
+			foreach (var provider in lockdown_providers.values) {
+				provider_unavailable (provider);
 				yield provider.close ();
 			}
-			provider_by_device_id.clear ();
-		}
+			lockdown_providers.clear ();
 
-		public async Fruity.Client create_client () {
-			if (!has_probed_protocol_version) {
-				bool service_is_present = false;
-
-				var client = new Fruity.ClientV1 ();
-				try {
-					yield client.establish ();
-					service_is_present = true;
-				} catch (IOError establish_error) {
-				}
-
-				if (service_is_present) {
-					try {
-						yield client.enable_listen_mode ();
-						protocol_version = 1;
-					} catch (IOError listen_error) {
-						protocol_version = 2;
-					}
-
-					has_probed_protocol_version = true;
-				}
+			foreach (var provider in remote_providers.values) {
+				provider_unavailable (provider);
+				yield provider.close ();
 			}
-
-			if (protocol_version == 1)
-				return new Fruity.ClientV1 ();
-			else
-				return new Fruity.ClientV2 ();
+			remote_providers.clear ();
 		}
 
-		private async void open_provider (FruityHostSessionProvider provider) {
-			try {
-				yield provider.open ();
+		private async void add_device (Fruity.DeviceDetails details) {
+			var id = details.id;
+			var raw_id = id.raw_value;
+			if (devices.contains (raw_id))
+				return;
+			devices.add (raw_id);
 
-				provider_available (provider);
-			} catch (Error e) {
-				provider_by_device_id.unset (provider.device_id);
-			}
-		}
-	}
+			string? name = null;
+			ImageData? icon_data = null;
 
-	public class FruityHostSessionProvider : Object, HostSessionProvider {
-		public string id {
-			get { return device_udid; }
-		}
-
-		public string name {
-			get { return _name; }
-		}
-		private string _name = "iOS Device";
-
-		public Image? icon {
-			get { return _icon; }
-		}
-		private Image? _icon = null;
-
-		public HostSessionProviderKind kind {
-			get { return HostSessionProviderKind.USB; }
-		}
-
-		public FruityHostSessionBackend backend {
-			get;
-			construct;
-		}
-
-		public uint device_id {
-			get;
-			construct;
-		}
-
-		public int device_product_id {
-			get;
-			construct;
-		}
-
-		public string device_udid {
-			get;
-			construct;
-		}
-
-		public bool is_open {
-			get;
-			private set;
-		}
-
-		private Gee.ArrayList<Entry> entries = new Gee.ArrayList<Entry> ();
-
-		private const uint DEFAULT_SERVER_PORT = 27042;
-
-		public FruityHostSessionProvider (FruityHostSessionBackend backend, uint device_id, int device_product_id, string device_udid) {
-			Object (backend: backend, device_id: device_id, device_product_id: device_product_id, device_udid: device_udid);
-		}
-
-		public async void open () throws Error {
 			bool got_details = false;
-			for (int i = 1; !got_details; i++) {
+			for (int i = 1; !got_details && devices.contains (raw_id); i++) {
 				try {
-					ImageData? icon_data;
-					_extract_details_for_device (device_product_id, device_udid, out _name, out icon_data);
-					_icon = Image.from_data (icon_data);
+					_extract_details_for_device (details.product_id.raw_value, details.udid.raw_value, out name, out icon_data);
 					got_details = true;
 				} catch (Error e) {
-					if (i != 60) {
+					if (i != 20) {
 						var source = new TimeoutSource.seconds (1);
 						source.set_callback (() => {
-							open.callback ();
+							add_device.callback ();
 							return false;
 						});
 						source.attach (MainContext.get_thread_default ());
@@ -209,174 +118,40 @@ namespace Frida {
 					}
 				}
 			}
+			if (!devices.contains (raw_id))
+				return;
+			if (!got_details) {
+				remove_device (id);
+				return;
+			}
 
-			if (!got_details)
-				throw new Error.TIMED_OUT ("Timed out while waiting for USB device to appear");
+			var icon = Image.from_data (icon_data);
 
-			is_open = true;
+			var remote_provider = new FruityRemoteProvider (name, icon, details);
+			remote_providers[raw_id] = remote_provider;
+
+			var lockdown_provider = new FruityLockdownProvider (name, icon, details);
+			lockdown_providers[raw_id] = lockdown_provider;
+
+			provider_available (remote_provider);
+			provider_available (lockdown_provider);
 		}
 
-		public async void close () {
-			foreach (var entry in entries)
-				yield destroy_entry (entry, SessionDetachReason.APPLICATION_REQUESTED);
-			entries.clear ();
-		}
+		private void remove_device (Fruity.DeviceId id) {
+			var raw_id = id.raw_value;
+			if (!devices.contains (raw_id))
+				return;
+			devices.remove (raw_id);
 
-		public async HostSession create (string? location = null) throws Error {
-			uint port = (location != null) ? (uint) int.parse (location) : DEFAULT_SERVER_PORT;
-			foreach (var entry in entries) {
-				if (entry.port == port)
-					throw new Error.INVALID_ARGUMENT ("Invalid location: already created");
-			}
+			FruityLockdownProvider lockdown_provider;
+			if (lockdown_providers.unset (raw_id, out lockdown_provider))
+				lockdown_provider.close.begin ();
 
-			Fruity.Client client = yield backend.create_client ();
-			DBusConnection connection;
-			try {
-				yield client.establish ();
-				yield client.connect_to_port (device_id, port);
-				connection = yield new DBusConnection (client.connection, null, DBusConnectionFlags.AUTHENTICATION_CLIENT);
-			} catch (GLib.Error e) {
-				if (e is IOError.CONNECTION_REFUSED)
-					throw new Error.SERVER_NOT_RUNNING ("Unable to connect to remote frida-server");
-				else
-					throw new Error.SERVER_NOT_RUNNING ("Unable to connect to remote frida-server: " + e.message);
-			}
-
-			HostSession session;
-			try {
-				session = yield connection.get_proxy (null, ObjectPath.HOST_SESSION);
-			} catch (IOError e) {
-				throw new Error.PROTOCOL ("Incompatible frida-server version");
-			}
-
-			var entry = new Entry (port, client, connection, session);
-			entry.agent_session_closed.connect (on_agent_session_closed);
-			entries.add (entry);
-
-			connection.on_closed.connect (on_connection_closed);
-
-			return session;
-		}
-
-		public async void destroy (HostSession host_session) throws Error {
-			foreach (var entry in entries) {
-				if (entry.host_session == host_session) {
-					entries.remove (entry);
-					yield destroy_entry (entry, SessionDetachReason.APPLICATION_REQUESTED);
-					return;
-				}
-			}
-			throw new Error.INVALID_ARGUMENT ("Invalid host session");
-		}
-
-		public async AgentSession obtain_agent_session (HostSession host_session, AgentSessionId agent_session_id) throws Error {
-			foreach (var entry in entries) {
-				if (entry.host_session == host_session)
-					return yield entry.obtain_agent_session (agent_session_id);
-			}
-			throw new Error.INVALID_ARGUMENT ("Invalid host session");
+			FruityRemoteProvider remote_provider;
+			if (remote_providers.unset (raw_id, out remote_provider))
+				remote_provider.close.begin ();
 		}
 
 		public extern static void _extract_details_for_device (int product_id, string udid, out string name, out ImageData? icon) throws Error;
-
-		private void on_connection_closed (DBusConnection connection, bool remote_peer_vanished, GLib.Error? error) {
-			bool closed_by_us = (!remote_peer_vanished && error == null);
-			if (closed_by_us)
-				return;
-
-			Entry entry_to_remove = null;
-			foreach (var entry in entries) {
-				if (entry.connection == connection) {
-					entry_to_remove = entry;
-					break;
-				}
-			}
-			assert (entry_to_remove != null);
-
-			entries.remove (entry_to_remove);
-			destroy_entry.begin (entry_to_remove, SessionDetachReason.SERVER_TERMINATED);
-		}
-
-		private void on_agent_session_closed (AgentSessionId id, SessionDetachReason reason, CrashInfo? crash) {
-			agent_session_closed (id, reason, crash);
-		}
-
-		private async void destroy_entry (Entry entry, SessionDetachReason reason) {
-			entry.connection.on_closed.disconnect (on_connection_closed);
-			yield entry.destroy (reason);
-			entry.agent_session_closed.disconnect (on_agent_session_closed);
-			host_session_closed (entry.host_session);
-		}
-
-		private class Entry : Object {
-			public signal void agent_session_closed (AgentSessionId id, SessionDetachReason reason, CrashInfo? crash);
-
-			public uint port {
-				get;
-				construct;
-			}
-
-			public Fruity.Client client {
-				get;
-				construct;
-			}
-
-			public DBusConnection connection {
-				get;
-				construct;
-			}
-
-			public HostSession host_session {
-				get;
-				construct;
-			}
-
-			private Gee.HashMap<AgentSessionId?, AgentSession> agent_session_by_id =
-				new Gee.HashMap<AgentSessionId?, AgentSession> (AgentSessionId.hash, AgentSessionId.equal);
-
-			public Entry (uint port, Fruity.Client client, DBusConnection connection, HostSession host_session) {
-				Object (port: port, client: client, connection: connection, host_session: host_session);
-
-				host_session.agent_session_destroyed.connect (on_agent_session_destroyed);
-				host_session.agent_session_crashed.connect (on_agent_session_crashed);
-			}
-
-			public async void destroy (SessionDetachReason reason) {
-				host_session.agent_session_crashed.disconnect (on_agent_session_crashed);
-				host_session.agent_session_destroyed.disconnect (on_agent_session_destroyed);
-
-				foreach (var agent_session_id in agent_session_by_id.keys)
-					agent_session_closed (agent_session_id, reason, null);
-				agent_session_by_id.clear ();
-
-				try {
-					yield connection.close ();
-				} catch (GLib.Error e) {
-				}
-			}
-
-			public async AgentSession obtain_agent_session (AgentSessionId id) throws Error {
-				AgentSession session = agent_session_by_id[id];
-				if (session == null) {
-					try {
-						session = yield connection.get_proxy (null, ObjectPath.from_agent_session_id (id));
-						agent_session_by_id[id] = session;
-					} catch (IOError proxy_error) {
-						throw new Error.INVALID_ARGUMENT (proxy_error.message);
-					}
-				}
-				return session;
-			}
-
-			private void on_agent_session_destroyed (AgentSessionId id, SessionDetachReason reason) {
-				if (agent_session_by_id.unset (id))
-					agent_session_closed (id, reason, null);
-			}
-
-			private void on_agent_session_crashed (AgentSessionId id, CrashInfo crash) {
-				agent_session_by_id.unset (id);
-				agent_session_closed (id, PROCESS_TERMINATED, crash);
-			}
-		}
 	}
 }

frida-core.vcxproj.filters
@@ -15,9 +15,8 @@
       <UniqueIdentifier>{93995380-89BD-4b04-88EB-625FBE52EBFB}</UniqueIdentifier>
       <Extensions>h;hpp;hxx;hm;inl;inc;xsd</Extensions>
     </Filter>
-    <Filter Include="Resource Files">
-      <UniqueIdentifier>{67DA6AB6-F800-4c08-8B7A-83BB121AAD01}</UniqueIdentifier>
-      <Extensions>rc;ico;cur;bmp;dlg;rc2;rct;bin;rgs;gif;jpg;jpeg;jpe;resx;tiff;tif;png;wav</Extensions>
+    <Filter Include="Header Files\generated">
+      <UniqueIdentifier>{0ddd771d-c7e1-405b-a3e6-a4433e9094db}</UniqueIdentifier>
     </Filter>
     <Filter Include="Source Files\windows">
       <UniqueIdentifier>{28f8e0ab-f994-4a5b-b62b-edcdcb9d0c34}</UniqueIdentifier>
@@ -25,11 +24,11 @@
     <Filter Include="Source Files\generated\windows">
       <UniqueIdentifier>{21f538e1-a14f-441b-9e2e-7303c1465c5b}</UniqueIdentifier>
     </Filter>
-    <Filter Include="Source Files\droidy">
-      <UniqueIdentifier>{7c397a46-f5dd-401d-b3c8-423615a25618}</UniqueIdentifier>
+    <Filter Include="Source Files\tcp">
+      <UniqueIdentifier>{d4552ab5-00ec-4394-bf75-666ac4f869a1}</UniqueIdentifier>
     </Filter>
-    <Filter Include="Source Files\generated\droidy">
-      <UniqueIdentifier>{2d5a13f8-9bcb-4d70-9f8a-9597707fc770}</UniqueIdentifier>
+    <Filter Include="Source Files\generated\tcp">
+      <UniqueIdentifier>{620f3726-c7dd-4d6f-8f54-ad08f2ff594a}</UniqueIdentifier>
     </Filter>
     <Filter Include="Source Files\fruity">
       <UniqueIdentifier>{7c397a46-f5dd-401d-b3c8-423615a25619}</UniqueIdentifier>
@@ -37,166 +36,187 @@
     <Filter Include="Source Files\generated\fruity">
       <UniqueIdentifier>{2d5a13f8-9bcb-4d70-9f8a-9597707fc771}</UniqueIdentifier>
     </Filter>
-    <Filter Include="Source Files\lib">
-      <UniqueIdentifier>{0913e432-8baa-452f-a325-f5ae232acab1}</UniqueIdentifier>
+    <Filter Include="Source Files\droidy">
+      <UniqueIdentifier>{7c397a46-f5dd-401d-b3c8-423615a25618}</UniqueIdentifier>
     </Filter>
-    <Filter Include="Source Files\lib\interfaces">
-      <UniqueIdentifier>{9729c4b0-4b37-4674-8f1b-8ff9601ef6f3}</UniqueIdentifier>
-    </Filter>
-    <Filter Include="Source Files\lib\agent">
-      <UniqueIdentifier>{9729c4b0-4b37-4674-8f1b-8ff9601ef6f4}</UniqueIdentifier>
-    </Filter>
-    <Filter Include="Source Files\generated\lib">
-      <UniqueIdentifier>{a0a6f4ce-c998-4406-81db-1b78adc3ab04}</UniqueIdentifier>
-    </Filter>
-    <Filter Include="Source Files\generated\lib\interfaces">
-      <UniqueIdentifier>{e0ca1c8c-d387-4bef-b6a6-d6c571db2eee}</UniqueIdentifier>
-    </Filter>
-    <Filter Include="Source Files\generated\lib\agent">
-      <UniqueIdentifier>{e0ca1c8c-d387-4bef-b6a6-d6c571db2eef}</UniqueIdentifier>
-    </Filter>
-    <Filter Include="Header Files\generated">
-      <UniqueIdentifier>{0ddd771d-c7e1-405b-a3e6-a4433e9094db}</UniqueIdentifier>
+    <Filter Include="Source Files\generated\droidy">
+      <UniqueIdentifier>{2d5a13f8-9bcb-4d70-9f8a-9597707fc770}</UniqueIdentifier>
     </Filter>
     <Filter Include="Source Files\generated\data">
       <UniqueIdentifier>{e441c3c0-5949-44e4-8617-0483732946e8}</UniqueIdentifier>
     </Filter>
-    <Filter Include="Source Files\generated\tcp">
-      <UniqueIdentifier>{620f3726-c7dd-4d6f-8f54-ad08f2ff594a}</UniqueIdentifier>
-    </Filter>
-    <Filter Include="Source Files\tcp">
-      <UniqueIdentifier>{d4552ab5-00ec-4394-bf75-666ac4f869a1}</UniqueIdentifier>
+    <Filter Include="Resource Files">
+      <UniqueIdentifier>{67DA6AB6-F800-4c08-8B7A-83BB121AAD01}</UniqueIdentifier>
+      <Extensions>rc;ico;cur;bmp;dlg;rc2;rct;bin;rgs;gif;jpg;jpeg;jpe;resx;tiff;tif;png;wav</Extensions>
     </Filter>
     <Filter Include="Resource Files\windows">
       <UniqueIdentifier>{c619adbc-c722-4220-8c57-9d40ce4e94ee}</UniqueIdentifier>
     </Filter>
   </ItemGroup>
   <ItemGroup>
-    <None Include="src\windows\winjector.vala">
-      <Filter>Source Files\windows</Filter>
-    </None>
-    <None Include="src\droidy\droidy-host-session.vala">
-      <Filter>Source Files\droidy</Filter>
-    </None>
-    <None Include="src\fruity\fruity-host-session.vala">
-      <Filter>Source Files\fruity</Filter>
-    </None>
-    <None Include="src\windows\windows-host-session.vala">
-      <Filter>Source Files\windows</Filter>
-    </None>
-    <None Include="src\tcp\tcp-host-session.vala">
-      <Filter>Source Files\tcp</Filter>
-    </None>
-    <None Include="src\droidy\droidy-client.vala">
-      <Filter>Source Files\droidy</Filter>
-    </None>
-    <None Include="src\fruity\fruity-client.vala">
-      <Filter>Source Files\fruity</Filter>
-    </None>
-    <None Include="src\fruity\fruity-property-list.vala">
-      <Filter>Source Files\fruity</Filter>
-    </None>
-    <None Include="src\host-session-service.vala">
+    <CustomBuild Include="src\windows\winjector.resources">
+      <Filter>Resource Files\windows</Filter>
+    </CustomBuild>
+    <CustomBuild Include="src\agent.resources">
+      <Filter>Resource Files</Filter>
+    </CustomBuild>
+    <CustomBuild Include="src\frida.vala">
+      <Filter>Source Files</Filter>
+    </CustomBuild>
+    <None Include="src\debugger.vala">
       <Filter>Source Files</Filter>
     </None>
-    <None Include="src\debugger.vala">
+    <None Include="src\host-session-service.vala">
       <Filter>Source Files</Filter>
     </None>
     <None Include="src\agent-container.vala">
       <Filter>Source Files</Filter>
     </None>
-  </ItemGroup>
-  <ItemGroup>
-    <ClCompile Include="$(IntDir)src\host-session-service.c">
-      <Filter>Source Files\generated\core</Filter>
-    </ClCompile>
-    <ClCompile Include="$(IntDir)src\windows\winjector.c">
-      <Filter>Source Files\generated\windows</Filter>
-    </ClCompile>
-    <ClCompile Include="src\windows\windows-host-session-glue.c">
+    <None Include="src\system.vala">
+      <Filter>Source Files</Filter>
+    </None>
+    <None Include="src\windows\windows-host-session.vala">
       <Filter>Source Files\windows</Filter>
-    </ClCompile>
-    <ClCompile Include="$(IntDir)src\droidy\droidy-host-session.c">
-      <Filter>Source Files\generated\droidy</Filter>
-    </ClCompile>
-    <ClCompile Include="$(IntDir)src\fruity\fruity-host-session.c">
-      <Filter>Source Files\generated\fruity</Filter>
-    </ClCompile>
-    <ClCompile Include="$(IntDir)src\windows\windows-host-session.c">
-      <Filter>Source Files\generated\windows</Filter>
-    </ClCompile>
-    <ClCompile Include="$(IntDir)src\system.c">
-      <Filter>Source Files\generated\core</Filter>
-    </ClCompile>
-    <ClCompile Include="$(IntDir)data\frida-data-agent.c">
-      <Filter>Source Files\generated\data</Filter>
-    </ClCompile>
-    <ClCompile Include="$(IntDir)data\frida-data-winjector.c">
-      <Filter>Source Files\generated\data</Filter>
-    </ClCompile>
-    <ClCompile Include="src\fruity\fruity-host-session-windows.c">
+    </None>
+    <None Include="src\windows\winjector.vala">
+      <Filter>Source Files\windows</Filter>
+    </None>
+    <None Include="src\windows\winjector-types.vala">
+      <Filter>Source Files\windows</Filter>
+    </None>
+    <None Include="src\tcp\tcp-host-session.vala">
+      <Filter>Source Files\tcp</Filter>
+    </None>
+    <None Include="src\fruity\fruity-host-session.vala">
       <Filter>Source Files\fruity</Filter>
-    </ClCompile>
-    <ClCompile Include="src\windows\winjector-glue.c">
-      <Filter>Source Files\windows</Filter>
-    </ClCompile>
-    <ClCompile Include="$(IntDir)src\tcp\tcp-host-session.c">
-      <Filter>Source Files\generated\tcp</Filter>
-    </ClCompile>
-    <ClCompile Include="$(IntDir)src\droidy\droidy-client.c">
-      <Filter>Source Files\generated\droidy</Filter>
-    </ClCompile>
-    <ClCompile Include="$(IntDir)src\fruity\fruity-client.c">
-      <Filter>Source Files\generated\fruity</Filter>
-    </ClCompile>
-    <ClCompile Include="$(IntDir)src\fruity\fruity-property-list.c">
-      <Filter>Source Files\generated\fruity</Filter>
-    </ClCompile>
-    <ClCompile Include="$(IntDir)src\frida.c">
-      <Filter>Source Files\generated\core</Filter>
-    </ClCompile>
-    <ClCompile Include="$(IntDir)src\debugger.c">
-      <Filter>Source Files\generated\core</Filter>
-    </ClCompile>
-    <ClCompile Include="$(IntDir)src\windows\winjector-types.c">
-      <Filter>Source Files\generated\windows</Filter>
-    </ClCompile>
-    <ClCompile Include="src\windows\system-windows.c">
-      <Filter>Source Files\windows</Filter>
-    </ClCompile>
-    <ClCompile Include="src\windows\icon-helpers.c">
-      <Filter>Source Files\windows</Filter>
-    </ClCompile>
-    <ClCompile Include="src\frida-glue.c">
-      <Filter>Source Files</Filter>
-    </ClCompile>
-    <ClCompile Include="src\windows\access-helpers.c">
-      <Filter>Source Files\windows</Filter>
-    </ClCompile>
-    <ClCompile Include="$(IntDir)src\agent-container.c">
-      <Filter>Source Files\generated\core</Filter>
-    </ClCompile>
-  </ItemGroup>
-  <ItemGroup>
-    <CustomBuild Include="src\windows\winjector.resources">
-      <Filter>Resource Files\windows</Filter>
-    </CustomBuild>
-    <CustomBuild Include="src\frida.vala">
-      <Filter>Source Files</Filter>
-    </CustomBuild>
-    <CustomBuild Include="src\agent.resources">
-      <Filter>Resource Files</Filter>
-    </CustomBuild>
+    </None>
+    <None Include="src\fruity\fruity-remote-provider.vala">
+      <Filter>Source Files\fruity</Filter>
+    </None>
+    <None Include="src\fruity\fruity-lockdown-provider.vala">
+      <Filter>Source Files\fruity</Filter>
+    </None>
+    <None Include="src\fruity\lockdown.vala">
+      <Filter>Source Files\fruity</Filter>
+    </None>
+    <None Include="src\fruity\installation-proxy.vala">
+      <Filter>Source Files\fruity</Filter>
+    </None>
+    <None Include="src\fruity\lldb.vala">
+      <Filter>Source Files\fruity</Filter>
+    </None>
+    <None Include="src\fruity\usbmux.vala">
+      <Filter>Source Files\fruity</Filter>
+    </None>
+    <None Include="src\fruity\plist.vala">
+      <Filter>Source Files\fruity</Filter>
+    </None>
+    <None Include="src\fruity\plist-service.vala">
+      <Filter>Source Files\fruity</Filter>
+    </None>
+    <None Include="src\droidy\droidy-host-session.vala">
+      <Filter>Source Files\droidy</Filter>
+    </None>
+    <None Include="src\droidy\droidy-client.vala">
+      <Filter>Source Files\droidy</Filter>
+    </None>
   </ItemGroup>
   <ItemGroup>
     <ClInclude Include="$(IntDir)frida-core.h">
       <Filter>Header Files\generated</Filter>
     </ClInclude>
-    <ClInclude Include="src\windows\icon-helpers.h">
+    <ClCompile Include="$(IntDir)src\frida.c">
+      <Filter>Source Files\generated\core</Filter>
+    </ClCompile>
+    <ClCompile Include="src\frida-glue.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)src\debugger.c">
+      <Filter>Source Files\generated\core</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)src\host-session-service.c">
+      <Filter>Source Files\generated\core</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)src\agent-container.c">
+      <Filter>Source Files\generated\core</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)src\system.c">
+      <Filter>Source Files\generated\core</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)src\windows\windows-host-session.c">
+      <Filter>Source Files\generated\windows</Filter>
+    </ClCompile>
+    <ClCompile Include="src\windows\windows-host-session-glue.c">
       <Filter>Source Files\windows</Filter>
-    </ClInclude>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)src\windows\winjector.c">
+      <Filter>Source Files\generated\windows</Filter>
+    </ClCompile>
+    <ClCompile Include="src\windows\winjector-glue.c">
+      <Filter>Source Files\windows</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)src\windows\winjector-types.c">
+      <Filter>Source Files\generated\windows</Filter>
+    </ClCompile>
     <ClInclude Include="src\windows\access-helpers.h">
       <Filter>Source Files\windows</Filter>
     </ClInclude>
+    <ClCompile Include="src\windows\access-helpers.c">
+      <Filter>Source Files\windows</Filter>
+    </ClCompile>
+    <ClInclude Include="src\windows\icon-helpers.h">
+      <Filter>Source Files\windows</Filter>
+    </ClInclude>
+    <ClCompile Include="src\windows\icon-helpers.c">
+      <Filter>Source Files\windows</Filter>
+    </ClCompile>
+    <ClCompile Include="src\windows\system-windows.c">
+      <Filter>Source Files\windows</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)src\tcp\tcp-host-session.c">
+      <Filter>Source Files\generated\tcp</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)src\fruity\fruity-host-session.c">
+      <Filter>Source Files\generated\fruity</Filter>
+    </ClCompile>
+    <ClCompile Include="src\fruity\fruity-host-session-windows.c">
+      <Filter>Source Files\fruity</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)src\fruity\fruity-remote-provider.c">
+      <Filter>Source Files\generated\fruity</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)src\fruity\fruity-lockdown-provider.c">
+      <Filter>Source Files\generated\fruity</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)src\fruity\lockdown.c">
+      <Filter>Source Files\generated\fruity</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)src\fruity\installation-proxy.c">
+      <Filter>Source Files\generated\fruity</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)src\fruity\lldb.c">
+      <Filter>Source Files\generated\fruity</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)src\fruity\usbmux.c">
+      <Filter>Source Files\generated\fruity</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)src\fruity\plist.c">
+      <Filter>Source Files\generated\fruity</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)src\fruity\plist-service.c">
+      <Filter>Source Files\generated\fruity</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)src\droidy\droidy-host-session.c">
+      <Filter>Source Files\generated\droidy</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)src\droidy\droidy-client.c">
+      <Filter>Source Files\generated\droidy</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)data\frida-data-winjector.c">
+      <Filter>Source Files\generated\data</Filter>
+    </ClCompile>
+    <ClCompile Include="$(IntDir)data\frida-data-agent.c">
+      <Filter>Source Files\generated\data</Filter>
+    </ClCompile>
   </ItemGroup>
 </Project>

commit 7988a362c15ec4e3284c480ee72ee6eae6d9cb26

Fix device removal wiring in the Fruity backend (#322)
This was a regression introduced in 95433be.

src/fruity/fruity-host-session.vala
@@ -167,8 +167,10 @@ namespace Frida {
 			devices.remove (raw_id);
 
 			FruityHostSessionProvider provider;
-			if (providers.unset (raw_id, out provider))
+			if (providers.unset (raw_id, out provider)) {
+				provider_unavailable (provider);
 				provider.close.begin (io_cancellable);
+			}
 		}
 
 		public extern static void _extract_details_for_device (int product_id, string udid, out string name, out ImageData? icon)