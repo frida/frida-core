a64e6f22bc179eff2ce898b46778422a50c3d190
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-07-06 18:56:55 +0200

    Add labrats for macOS/arm64(e)

---
+++ tests/labrats/merge-macos-slices.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -ex
+
+for f in *-macos*; do
+  echo "Patching $f"
+  lipo ../$f -thin i386 -output $f.i386
+  lipo ../$f -thin x86_64 -output $f.x86_64
+  lipo $f -thin arm64 -output $f.arm64
+  lipo $f -thin arm64e -output $f.arm64e
+  lipo $f.i386 $f.x86_64 $f.arm64 $f.arm64e -create -output ../$f
+  rm $f.i386 $f.x86_64 $f.arm64 $f.arm64e
+done
