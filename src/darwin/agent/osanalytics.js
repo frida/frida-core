2a1f268a7c00a2770ec6248904aeb13528d7423d
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-11-04 04:20:17 +0100

    Fix early instrumentation of osanalyticshelper on iOS
    So the crash reporter integration always works on iOS 14.

--- src/darwin/agent/osanalytics.js
+++ src/darwin/agent/osanalytics.js
@@ -8,38 +8,53 @@ const {
 
 const sessions = new Map();
 
-Interceptor.attach(OSALog['+ locallyCreateForSubmission:metadata:options:error:writing:'].implementation, {
-  onEnter(args) {
-    sessions.set(this.threadId, {
-      forcedByUs: false
-    });
-  },
-  onLeave(retval) {
-    const log = new ObjC.Object(retval);
-    const { threadId } = this;
+function tryInitialize() {
+  const counterMethod = NSMutableDictionary['- osa_logCounter_isLog:byKey:count:withinLimit:withOptions:'];
+  if (counterMethod === undefined)
+    return false;
 
-    const session = sessions.get(threadId);
-    if (session.forcedByUs) {
-      const oldPath = log.filepath().toString();
-      const newPath = oldPath + '.forced-by-frida';
-      NSFileManager.defaultManager().moveItemAtPath_toPath_error_(oldPath, newPath, NULL);
-      log.rename_(newPath);
-    }
+  Interceptor.attach(OSALog['+ locallyCreateForSubmission:metadata:options:error:writing:'].implementation, {
+    onEnter(args) {
+      sessions.set(this.threadId, {
+        forcedByUs: false
+      });
+    },
+    onLeave(retval) {
+      const log = new ObjC.Object(retval);
+      const { threadId } = this;
 
-    sessions.delete(threadId);
-  },
-});
+      const session = sessions.get(threadId);
+      if (session.forcedByUs) {
+        const oldPath = log.filepath().toString();
+        const newPath = oldPath + '.forced-by-frida';
+        NSFileManager.defaultManager().moveItemAtPath_toPath_error_(oldPath, newPath, NULL);
+        log.rename_(newPath);
+      }
 
-Interceptor.attach(NSMutableDictionary['- osa_logCounter_isLog:byKey:count:withinLimit:withOptions:'].implementation, {
-  onLeave(retval) {
-    const session = sessions.get(this.threadId);
-    if (session === undefined)
-      return;
+      sessions.delete(threadId);
+    },
+  });
 
-    const isWithinLimit = !!retval.toInt32();
-    if (!isWithinLimit) {
-      retval.replace(YES);
-      session.forcedByUs = true;
-    }
-  },
-});
+  Interceptor.attach(counterMethod.implementation, {
+    onLeave(retval) {
+      const session = sessions.get(this.threadId);
+      if (session === undefined)
+        return;
+
+      const isWithinLimit = !!retval.toInt32();
+      if (!isWithinLimit) {
+        retval.replace(YES);
+        session.forcedByUs = true;
+      }
+    },
+  });
+
+  return true;
+}
+
+if (!tryInitialize()) {
+  const listener = Interceptor.attach(Module.getExportByName('/usr/lib/system/libsystem_trace.dylib', 'os_log_type_enabled'), () => {
+    tryInitialize();
+    listener.detach();
+  });
+}
