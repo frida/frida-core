b3817cd3b640b72515a397aa159e91fdabf98f2f
Ole André Vadla Ravnås <oleavr@gmail.com>
2024-05-09 09:08:00 +0200

    Revert "compat: Add temporary debug"
    This reverts commit 81b384bba0d02304c7bf33e446a0f59916ef3ce9.

--- compat/build.py
+++ compat/build.py
@@ -429,17 +429,10 @@ def compile(privdir: Path, state: State):
                                          check=True).stdout.rstrip().split("\n")
             input_entries = []
             for raw_path in input_paths:
-                print("raw_path:", raw_path)
                 path = Path(raw_path)
                 if not path.is_absolute():
-                    abspath = workdir / path
-                    print(f"\tcomputing relative for: {abspath}")
-                    raw_relpath = os.path.relpath(abspath, top_builddir)
-                    print(f"\traw_relpath: {raw_relpath}")
-                    path = Path(raw_relpath)
-                x = quote(path.as_posix())
-                print(f"\t=> {x}")
-                input_entries.append(x)
+                    path = Path(os.path.relpath(workdir / path, top_builddir))
+                input_entries.append(quote(path.as_posix()))
             depfile_lines.append(f"{output_relpath}: {' '.join(input_entries)}")
 
     (state.builddir / DEPFILE_FILENAME).write_text("\n".join(depfile_lines), encoding="utf-8")
