d43e68cc4e27eb442dbb8f158266a7efaf3e2875
Ole André Vadla Ravnås <oleavr@gmail.com>
2024-07-23 10:08:34 +0200

    Tweak style

--- src/fruity/device-monitor-windows.c
+++ src/fruity/device-monitor-windows.c
@@ -121,14 +121,15 @@ _frida_fruity_windows_pairing_browser_enumerate_network_interfaces (FridaFruityW
 
   for (adapter = adapters; adapter != NULL; adapter = adapter->Next)
   {
-    if (adapter->FirstUnicastAddress == NULL)
-      continue;
-
-    SOCKET_ADDRESS * raw_addr = &adapter->FirstUnicastAddress->Address;
+    SOCKET_ADDRESS * raw_addr;
     GInetSocketAddress * addr;
 
     if (adapter->IfType == IF_TYPE_SOFTWARE_LOOPBACK)
       continue;
+
+    if (adapter->FirstUnicastAddress == NULL)
+      continue;
+    raw_addr = &adapter->FirstUnicastAddress->Address;
 
     addr = G_INET_SOCKET_ADDRESS (g_socket_address_new_from_native (raw_addr->lpSockaddr, raw_addr->iSockaddrLength));
 
