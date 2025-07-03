cb039a659bcf38da1f5b158bacb1e2dc9337adef
Ole André Vadla Ravnås <oleavr@gmail.com>
2025-07-04 01:29:19 +0200

    barebone: Wire up more agent bits

--- src/barebone/agent/src/main.rs
+++ src/barebone/agent/src/main.rs
@@ -1,19 +1,41 @@
+#![no_main]
+#![no_std]
+
 mod bindings {
-    #![allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
+    #![allow(non_camel_case_types, non_snake_case, non_upper_case_globals, dead_code)]
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
 
 use bindings::*;
-use std::ptr;
+use core::ffi::CStr;
 
-fn main() {
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn _start() -> i32 {
+    let mut sum: i32 = 0;
+
     unsafe {
         let rt = JS_NewRuntime();
-        if rt.is_null() {
-            eprintln!("Failed to create JS runtime");
-        } else {
-            println!("QuickJS runtime created!");
-            JS_FreeRuntime(rt);
-        }
+        let ctx = JS_NewContext(rt);
+
+        let name_str = "sum.js\0";
+        let code_str = "3 + 4;\0";
+
+        let name = CStr::from_bytes_with_nul(name_str.as_bytes()).unwrap();
+        let code = CStr::from_bytes_with_nul(code_str.as_bytes()).unwrap();
+
+        let sum_val = JS_Eval(ctx, code.as_ptr(), code_str.len() - 1, name.as_ptr(),
+            (JS_EVAL_TYPE_GLOBAL | JS_EVAL_FLAG_STRICT) as i32);
+
+        JS_ToInt32(ctx, &mut sum, sum_val);
+
+        JS_FreeContext(ctx);
+        JS_FreeRuntime(rt);
     }
+
+    sum
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo<'_>) -> ! {
+    loop {}
 }
