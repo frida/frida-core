e60f7060d8aee37c0f359a3e6977f858bd866f25
Ole André Vadla Ravnås <oleavr@gmail.com>
2025-07-06 15:56:58 +0200

    barebone: Wire up more agent bits

--- src/barebone/agent/explore.ts
+++ src/barebone/agent/explore.ts
@@ -3,5 +3,6 @@ const cm = new CModule(File.readAllBytes("./target/aarch64-unknown-none/release/
 const start = new NativeFunction(cm._start, "pointer", []);
 const bufferPhysicalAddress = start();
 console.log("Ready! Buffer is at physical address:", bufferPhysicalAddress);
+$gdb.continue();
 
 Object.assign(globalThis, { cm, start });
