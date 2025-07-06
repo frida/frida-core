4a31adcd2ce5ea02872b466e2d70f9ac925ddcb6
Ole André Vadla Ravnås <oleavr@gmail.com>
2025-07-06 13:03:43 +0200

    agent: Add example configs

--- src/barebone/agent/env.sh
+++ src/barebone/agent/env.sh
@@ -1,3 +1,6 @@
+export FRIDA_BAREBONE_CONFIG=$PWD/etc/xnu.config
+export PYTHONPATH=$HOME/src/frida-python
+
 export PATH=$PWD/xpacks/.bin:$PATH
 export CC_aarch64_unknown_none=aarch64-none-elf-gcc
 export AR_aarch64_unknown_none=aarch64-none-elf-ar
