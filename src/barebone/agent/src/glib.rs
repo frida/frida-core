ebf3a297ba25c35dd71628502d4833be70be062d
Ole André Vadla Ravnås <oleavr@gmail.com>
2025-07-30 17:53:31 +0200

    WIP

--- src/barebone/agent/src/glib.rs
+++ src/barebone/agent/src/glib.rs
@@ -1,12 +1,11 @@
 use core::ptr;
-use core::ffi::c_void;
 
 use crate::bindings::{g_wait_is_set, gint64, gpointer};
 use crate::{kprintln, xnu};
 
 const G_WAIT_INFINITE: gint64 = -1;
 
-static mut PENDING_EVENT: u64 = 0;
+pub static mut WAKEUP_TOKEN: u64 = 0;
 static mut DOORBELL_INTERRUPT_COUNT: u64 = 0;
 
 #[unsafe(no_mangle)]
@@ -18,7 +17,7 @@ pub extern "C" fn g_get_monotonic_time() -> gint64 {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn g_wait_sleep(token: gpointer, timeout_us: gint64) {
-    let wait_event = ptr::addr_of_mut!(PENDING_EVENT) as *const u8;
+    let wait_event = ptr::addr_of_mut!(WAKEUP_TOKEN) as *const u8;
 
     let wait_result = if timeout_us == G_WAIT_INFINITE {
         xnu::assert_wait(wait_event, xnu::THREAD_INTERRUPTIBLE)
@@ -48,44 +47,5 @@ pub extern "C" fn g_wait_sleep(token: gpointer, timeout_us: gint64) {
 #[unsafe(no_mangle)]
 pub extern "C" fn g_wait_wake(_token: gpointer) {
     kprintln!("[FRIDA] g_wait_wake: waking up the event!");
-    xnu::thread_wakeup(ptr::addr_of_mut!(PENDING_EVENT) as *const u8);
-}
-
-const DOORBELL_PADDR: u64 = 0x200100000;
-const DOORBELL_SIZE: u64 = 0x4000;
-
-const REG_IRQ: usize = 0x0; // R 32‑bit
-
-pub fn init_host_doorbell() {
-    unsafe {
-        let doorbell_va = xnu::ml_io_map(DOORBELL_PADDR, DOORBELL_SIZE);
-
-        kprintln!(
-            "[FRIDA] Host doorbell PA={:#x} VA={:#x}, ",
-            DOORBELL_PADDR,
-            doorbell_va as u64
-        );
-
-        let irq = ptr::read_volatile((doorbell_va as u64 + REG_IRQ as u64) as *const u64) as i32;
-        kprintln!("[FRIDA] Host doorbell irq={}", irq);
-
-        xnu::install_interrupt_handler(
-            irq,
-            ptr::addr_of_mut!(PENDING_EVENT) as *mut c_void,
-            on_doorbell_interrupt,
-            core::ptr::null_mut(),
-        );
-    }
-}
-
-extern "C" fn on_doorbell_interrupt(
-    target: *mut c_void,
-    _refcon: *mut c_void,
-    _nub: *mut c_void,
-    _source: i32,
-) {
-    unsafe {
-        DOORBELL_INTERRUPT_COUNT += 1;
-    }
-    xnu::thread_wakeup(target as *const u8);
+    xnu::thread_wakeup(ptr::addr_of_mut!(WAKEUP_TOKEN) as *const u8);
 }
