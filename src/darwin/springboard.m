frida/frida-core

commit 8108d4d95449e334518ff96ef5ee141bbf873ec3

Plug Interceptor singleton leaks (#1167)

src/darwin/springboard.m
@@ -109,6 +109,8 @@ _frida_get_springboard_api (void)
         gum_interceptor_replace (interceptor, frida_xpc_look_up_endpoint, frida_replacement_xpc_look_up_endpoint, NULL, NULL);
       else
         g_error ("Unable to locate _xpc_look_up_endpoint(); please file a bug");
+
+      g_object_unref (interceptor);
     }
 #endif
 

lib/payload/unwind-sitter-glue.c
@@ -55,6 +55,7 @@ struct _FridaUnwindHookState
   gpointer set_info_original;
   void (* set_info) (gpointer cursor, gint is_return_address);
   gpointer (* get_reg) (gpointer cursor, gint reg);
+  GumInterceptor * interceptor;
 };
 
 #if __has_feature (ptrauth_calls)
@@ -97,7 +98,6 @@ _frida_unwind_sitter_hook_libunwind (void)
 #if GLIB_SIZEOF_VOID_P == 8
   gpointer * set_info_slot;
   gpointer get_reg_impl;
-  GumInterceptor * interceptor;
 
   if (state != NULL)
     return;
@@ -121,8 +121,9 @@ _frida_unwind_sitter_hook_libunwind (void)
   state->set_info = FRIDA_RESIGN_PTR (state->set_info_original);
   state->get_reg = FRIDA_RESIGN_PTR (get_reg_impl);
 
-  interceptor = gum_interceptor_obtain ();
-  if (gum_interceptor_replace (interceptor, state->set_info_original, frida_unwind_cursor_set_info_replacement, NULL, NULL)
+  state->interceptor = gum_interceptor_obtain ();
+
+  if (gum_interceptor_replace (state->interceptor, state->set_info_original, frida_unwind_cursor_set_info_replacement, NULL, NULL)
       != GUM_REPLACE_OK)
     goto unsupported_version;
 
@@ -137,13 +138,12 @@ unsupported_version:
 void
 _frida_unwind_sitter_unhook_libunwind (void)
 {
-  GumInterceptor * interceptor;
-
   if (state == NULL)
     return;
 
-  interceptor = gum_interceptor_obtain ();
-  gum_interceptor_revert (interceptor, state->set_info_original);
+  gum_interceptor_revert (state->interceptor, state->set_info_original);
+
+  g_object_unref (state->interceptor);
 
   g_slice_free (FridaUnwindHookState, state);
   state = NULL;

commit cc953751360128e5bc263e56d962ef56a62c0f48

darwin: Revive app listing/launching on iOS >= 16
When running on rootful systems. This brings dccb612 back to life.
It was broken in 8108d4d, which fixed two Interceptor singleton leaks.
Turns out the one in springboard.m was intentional, as it was done as
part of init-once logic, with no teardown intended. And since nothing
else uses Interceptor in this case, the instrumentation we just
applied gets reverted right away.

Kudo to @alexhude for reporting.

src/darwin/springboard.m
@@ -109,8 +109,6 @@ _frida_get_springboard_api (void)
         gum_interceptor_replace (interceptor, frida_xpc_look_up_endpoint, frida_replacement_xpc_look_up_endpoint, NULL, NULL);
       else
         g_error ("Unable to locate _xpc_look_up_endpoint(); please file a bug");
-
-      g_object_unref (interceptor);
     }
 #endif
 