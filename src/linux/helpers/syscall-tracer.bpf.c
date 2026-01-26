fb91a7c2426f435fec5daac9cedbc461c12e1fde
Ole André Vadla Ravnås <oleavr@frida.re>
2026-01-26 22:18:09 +0100

    Wire up some more
    Co-authored-by: Håvard Sørbø <havard@hsorbo.no>

--- src/linux/helpers/syscall-tracer.bpf.c
+++ src/linux/helpers/syscall-tracer.bpf.c
@@ -6,33 +6,30 @@
 
 #define MAX_DEPTH 16
 #define MAX_STACK_ENTRIES 16384
-#define MAX_READLINKAT_ARGS_ENTRIES 4096
-#define MAX_PATH  256
-#define MAX_SOCK  128
 
-typedef __u16 SyscallEventKind;
-typedef __u16 SyscallEventPhase;
-typedef struct _SyscallEvent SyscallEvent;
-typedef struct _SyscallOpenatPayload SyscallOpenatPayload;
-typedef struct _SyscallFaccessatPayload SyscallFaccessatPayload;
-typedef struct _SyscallStatfsPayload SyscallStatfsPayload;
-typedef struct _SyscallNewfstatatPayload SyscallNewfstatatPayload;
-typedef struct _SyscallReadlinkatPayload SyscallReadlinkatPayload;
-typedef struct _SyscallReadlinkatExitPayload SyscallReadlinkatExitPayload;
-typedef struct _SyscallConnectPayload SyscallConnectPayload;
+#define MAX_INFLIGHT_ENTRIES 4096
 
-typedef struct _ReadlinkatArgs ReadlinkatArgs;
+#define MAX_PATH 256
+#define MAX_SOCK 128
 
-enum _SyscallEventKind
-{
-  SYSCALL_EVENT_GENERIC,
-  SYSCALL_EVENT_OPENAT,
-  SYSCALL_EVENT_FACCESSAT,
-  SYSCALL_EVENT_STATFS,
-  SYSCALL_EVENT_NEWFSTATAT,
-  SYSCALL_EVENT_READLINKAT,
-  SYSCALL_EVENT_CONNECT,
-};
+#define SYSCALL_NARGS 6
+
+typedef __u16 SyscallPhase;
+typedef struct _SyscallEventCommon SyscallEventCommon;
+typedef struct _SyscallEnterPayload SyscallEnterPayload;
+typedef struct _SyscallExitPayload SyscallExitPayload;
+
+typedef __u16 AttachType;
+typedef struct _AttachHeader AttachHeader;
+
+typedef struct _SyscallEnterEventNone SyscallEnterEventNone;
+typedef struct _SyscallEnterEventPath SyscallEnterEventPath;
+typedef struct _SyscallEnterEventSock SyscallEnterEventSock;
+
+typedef struct _SyscallExitEventNone SyscallExitEventNone;
+typedef struct _SyscallExitEventOut SyscallExitEventOut;
+
+typedef struct _Inflight Inflight;
 
 enum
 {
@@ -40,7 +37,7 @@ enum
   SYSCALL_PHASE_EXIT,
 };
 
-struct _SyscallEvent
+struct _SyscallEventCommon
 {
   __u64 time_ns;
   __u32 tgid;
@@ -49,75 +46,99 @@ struct _SyscallEvent
   __s32 syscall_nr;
   __s32 stack_id;
 
-  SyscallEventKind kind;
+  SyscallPhase phase;
+
   __u16 payload_len;
+  __u16 attach_count;
+};
 
-  SyscallEventPhase phase;
+struct _SyscallEnterPayload
+{
+  __u64 args[SYSCALL_NARGS];
+};
+
+struct _SyscallExitPayload
+{
   __s64 retval;
-
-  /* payload follows */
 };
 
-struct _SyscallOpenatPayload
+enum
 {
-  __s32 dfd;
-  __s32 flags;
-  __u32 mode;
-  __u32 path_len;
-  char  path[MAX_PATH];
+  ATTACH_STRING_ARG,
+  ATTACH_BYTES_ARG,
+  ATTACH_OUT_BYTES,
 };
 
-struct _SyscallFaccessatPayload
+struct _AttachHeader
 {
-  __s32 dfd;
-  __s32 mode;
-  __s32 flags;
-  __u32 path_len;
-  char  path[MAX_PATH];
+  AttachType type;
+  __u16 arg_index;
+  __u32 len;
 };
 
-struct _SyscallStatfsPayload
+struct _SyscallEnterEventNone
 {
-  __u32 path_len;
-  char  path[MAX_PATH];
+  SyscallEventCommon common;
+  SyscallEnterPayload payload;
 };
 
-struct _SyscallNewfstatatPayload
+struct _SyscallEnterEventPath
 {
-  __s32 dfd;
-  __s32 flags;
-  __u32 path_len;
-  char  path[MAX_PATH];
+  SyscallEventCommon common;
+  SyscallEnterPayload payload;
+
+  AttachHeader attach;
+  __u8 data[MAX_PATH];
 };
 
-struct _SyscallReadlinkatPayload
+struct _SyscallEnterEventSock
 {
-  __s32 dfd;
-  __u32 bufsize;
-  __u32 path_len;
-  char  path[MAX_PATH];
+  SyscallEventCommon common;
+  SyscallEnterPayload payload;
+
+  AttachHeader attach;
+  __u8 data[MAX_SOCK];
 };
 
-struct _SyscallReadlinkatExitPayload
+struct _SyscallExitEventNone
 {
-  __u32 link_len;
-  char  link[MAX_PATH];
+  SyscallEventCommon common;
+  SyscallExitPayload payload;
 };
 
-struct _SyscallConnectPayload
+struct _SyscallExitEventOut
 {
-  __s32 fd;
-  __u32 addrlen;
-  __u16 family;
-  __u16 _pad;
-  __u8  addr[MAX_SOCK];
+  SyscallEventCommon common;
+  SyscallExitPayload payload;
+
+  AttachHeader attach;
+  __u8 data[MAX_PATH];
 };
 
-struct _ReadlinkatArgs
+struct _Inflight
 {
-  __u64 buf;
-  __u32 bufsize;
-  __u32 _pad;
+  __s32 syscall_nr;
+
+  __u16 kind;
+  __u16 _pad0;
+
+  union
+  {
+    struct
+    {
+      __u16 arg_index;
+      __u16 _pad1;
+
+      __u64 user_ptr;
+      __u32 max_len;
+      __u32 _pad2;
+    } out_copy;
+  } u;
+};
+
+enum
+{
+  INFLIGHT_KIND_OUT_COPY = 1,
 };
 
 struct
@@ -148,11 +169,11 @@ stacks SEC (".maps");
 struct
 {
   __uint (type, BPF_MAP_TYPE_HASH);
-  __uint (max_entries, MAX_READLINKAT_ARGS_ENTRIES);
+  __uint (max_entries, MAX_INFLIGHT_ENTRIES);
   __type (key, __u32);
-  __type (value, ReadlinkatArgs);
+  __type (value, Inflight);
 }
-readlinkat_args SEC (".maps");
+inflight SEC (".maps");
 
 struct trace_event_raw_sys_enter
 {
@@ -168,11 +189,21 @@ struct trace_event_raw_sys_exit
   long ret;
 };
 
-static __always_inline SyscallEventKind classify_syscall (__s32 nr);
-static __always_inline __u32 clamp_u32 (__u32 v, __u32 max);
+static __always_inline SyscallEnterEventNone * reserve_enter_none (void);
+static __always_inline SyscallEnterEventPath * reserve_enter_path (void);
+static __always_inline SyscallEnterEventSock * reserve_enter_sock (void);
 
-static __always_inline SyscallEvent *
-reserve_event (SyscallEventKind kind, __u16 payload_len);
+static __always_inline SyscallExitEventNone * reserve_exit_none (void);
+static __always_inline SyscallExitEventOut * reserve_exit_out (void);
+
+static __always_inline void fill_common (SyscallEventCommon * e, __u32 tgid, __u32 tid, __s32 nr, SyscallPhase phase, void * ctx);
+static __always_inline void fill_enter_args (SyscallEnterPayload * p, struct trace_event_raw_sys_enter * ctx);
+
+static __always_inline __u16 write_attach_str_arg (AttachHeader * h, __u16 arg_index, __u8 * dst, __u32 dst_cap, const void * user_str);
+static __always_inline __u16 write_attach_bytes_arg (AttachHeader * h, __u16 arg_index, __u8 * dst, __u32 dst_cap, const void * user_src,
+    __u32 n);
+
+static __always_inline void maybe_schedule_out_copy ( __u32 tid, __s32 nr, __u16 arg_index, __u64 user_ptr, __u32 max_len );
 
 SEC ("tracepoint/raw_syscalls/sys_enter")
 int
@@ -190,147 +221,76 @@ on_sys_enter (struct trace_event_raw_sys_enter * ctx)
     return 0;
 
   __s32 nr = (__s32) ctx->id;
-  SyscallEventKind kind = classify_syscall (nr);
 
-  __u16 payload_len = 0;
-
-  switch (kind)
+  if (nr == FRIDA_LINUX_SYSCALL_OPENAT ||
+      nr == FRIDA_LINUX_SYSCALL_FACCESSAT ||
+      nr == FRIDA_LINUX_SYSCALL_STATFS ||
+#ifdef FRIDA_LINUX_SYSCALL_NEWFSTATAT
+      nr == FRIDA_LINUX_SYSCALL_NEWFSTATAT ||
+#endif
+      nr == FRIDA_LINUX_SYSCALL_READLINKAT)
   {
-    case SYSCALL_EVENT_OPENAT:
-      payload_len = (__u16) sizeof (SyscallOpenatPayload);
-      break;
-    case SYSCALL_EVENT_FACCESSAT:
-      payload_len = (__u16) sizeof (SyscallFaccessatPayload);
-      break;
-    case SYSCALL_EVENT_STATFS:
-      payload_len = (__u16) sizeof (SyscallStatfsPayload);
-      break;
-    case SYSCALL_EVENT_NEWFSTATAT:
-      payload_len = (__u16) sizeof (SyscallNewfstatatPayload);
-      break;
-    case SYSCALL_EVENT_READLINKAT:
-      payload_len = (__u16) sizeof (SyscallReadlinkatPayload);
-      break;
-    case SYSCALL_EVENT_CONNECT:
-      payload_len = (__u16) sizeof (SyscallConnectPayload);
-      break;
-    default:
-      payload_len = 0;
-      break;
-  }
+    SyscallEnterEventPath * ev = reserve_enter_path ();
+    if (ev == NULL)
+      return 0;
 
-  SyscallEvent * e = reserve_event (kind, payload_len);
-  if (e == NULL)
+    fill_common (&ev->common, tgid, tid, nr, SYSCALL_PHASE_ENTER, ctx);
+    fill_enter_args (&ev->payload, ctx);
+
+    __u16 arg_index = 1;
+    if (nr == FRIDA_LINUX_SYSCALL_STATFS)
+      arg_index = 0;
+
+    __u16 used;
+    if (nr == FRIDA_LINUX_SYSCALL_STATFS)
+      used = write_attach_str_arg (&ev->attach, 0, &ev->data[0], MAX_PATH, (void *) ctx->args[0]);
+    else
+      used = write_attach_str_arg (&ev->attach, 1, &ev->data[0], MAX_PATH, (void *) ctx->args[1]);
+
+    ev->common.payload_len = ( __u16 ) (sizeof (SyscallEnterPayload) + sizeof (AttachHeader) + used);
+    ev->common.attach_count = 1;
+
+    if (nr == FRIDA_LINUX_SYSCALL_READLINKAT)
+    {
+      maybe_schedule_out_copy (tid, nr, 2, (__u64) ctx->args[2], (__u32) ctx->args[3]);
+    }
+
+    bpf_ringbuf_submit (ev, 0);
     return 0;
-
-  e->time_ns = bpf_ktime_get_ns ();
-  e->tgid = tgid;
-  e->tid = tid;
-  e->syscall_nr = nr;
-  e->kind = kind;
-  e->payload_len = payload_len;
-
-  e->phase = SYSCALL_PHASE_ENTER;
-  e->retval = 0;
-
-  e->stack_id = bpf_get_stackid (ctx, &stacks, BPF_F_USER_STACK);
-
-  void * payload = (void *) (e + 1);
-
-  if (kind == SYSCALL_EVENT_OPENAT)
-  {
-    SyscallOpenatPayload * p = payload;
-
-    p->dfd = (__s32) ctx->args[0];
-    p->flags = (__s32) ctx->args[2];
-    p->mode = (__u32) ctx->args[3];
-
-    long r = bpf_probe_read_user_str (p->path, MAX_PATH, (void *) ctx->args[1]);
-    p->path_len = (r > 0) ? r : 0;
   }
-  else if (kind == SYSCALL_EVENT_FACCESSAT)
+
+  if (nr == FRIDA_LINUX_SYSCALL_CONNECT)
   {
-    SyscallFaccessatPayload * p = payload;
+    SyscallEnterEventSock * ev = reserve_enter_sock ();
+    if (ev == NULL)
+      return 0;
 
-    p->dfd = (__s32) ctx->args[0];
-    long r = bpf_probe_read_user_str (p->path, MAX_PATH, (void *) ctx->args[1]);
-    p->path_len = (r > 0) ? r : 0;
-    p->mode = (__s32) ctx->args[2];
-    p->flags = (__s32) ctx->args[3];
-  }
-  else if (kind == SYSCALL_EVENT_STATFS)
-  {
-    SyscallStatfsPayload * p = payload;
-
-    long r = bpf_probe_read_user_str (p->path, MAX_PATH, (void *) ctx->args[0]);
-    p->path_len = (r > 0) ? r : 0;
-  }
-  else if (kind == SYSCALL_EVENT_NEWFSTATAT)
-  {
-    SyscallNewfstatatPayload * p = payload;
-
-    p->dfd = (__s32) ctx->args[0];
-    long r = bpf_probe_read_user_str (p->path, MAX_PATH, (void *) ctx->args[1]);
-    p->path_len = (r > 0) ? r : 0;
-
-    p->flags = (__s32) ctx->args[3];
-  }
-  else if (kind == SYSCALL_EVENT_READLINKAT)
-  {
-    SyscallReadlinkatPayload * p = payload;
-
-    p->dfd = (__s32) ctx->args[0];
-    long r = bpf_probe_read_user_str (p->path, MAX_PATH, (void *) ctx->args[1]);
-    p->path_len = (r > 0) ? r : 0;
-
-    p->bufsize = (__u32) ctx->args[3];
-
-    ReadlinkatArgs a;
-    a.buf = (__u64) ctx->args[2];
-    a.bufsize = (__u32) ctx->args[3];
-    a._pad = 0;
-    bpf_map_update_elem (&readlinkat_args, &tid, &a, BPF_ANY);
-  }
-  else if (kind == SYSCALL_EVENT_CONNECT)
-  {
-    SyscallConnectPayload * p = payload;
-
-    p->fd = (__s32) ctx->args[0];
+    fill_common (&ev->common, tgid, tid, nr, SYSCALL_PHASE_ENTER, ctx);
+    fill_enter_args (&ev->payload, ctx);
 
     __u32 n = (__u32) ctx->args[2];
+    __u16 used = write_attach_bytes_arg (&ev->attach, 1, &ev->data[0], MAX_SOCK, (void *) ctx->args[1], n);
 
-    p->family = 0;
-    p->_pad = 0;
+    ev->common.payload_len = ( __u16 ) (sizeof (SyscallEnterPayload) + sizeof (AttachHeader) + used);
+    ev->common.attach_count = 1;
 
-    if (n > MAX_SOCK)
-    {
-      p->addrlen = MAX_SOCK;
-      bpf_probe_read_user (p->addr, MAX_SOCK, (void *) ctx->args[1]);
-
-      __u16 fam = 0;
-      bpf_probe_read_kernel (&fam, sizeof (fam), p->addr);
-      p->family = fam;
-    }
-    else if (n != 0)
-    {
-      p->addrlen = n;
-      bpf_probe_read_user (p->addr, n, (void *) ctx->args[1]);
-
-      if (n >= sizeof (__u16))
-      {
-        __u16 fam = 0;
-        bpf_probe_read_kernel (&fam, sizeof (fam), p->addr);
-        p->family = fam;
-      }
-    }
-    else
-    {
-      p->addrlen = 0;
-    }
+    bpf_ringbuf_submit (ev, 0);
+    return 0;
   }
 
-  bpf_ringbuf_submit (e, 0);
-  return 0;
+  {
+    SyscallEnterEventNone * ev = reserve_enter_none ();
+    if (ev == NULL)
+      return 0;
+
+    fill_common (&ev->common, tgid, tid, nr, SYSCALL_PHASE_ENTER, ctx);
+    fill_enter_args (&ev->payload, ctx);
+
+    ev->common.payload_len = ( __u16 ) sizeof (SyscallEnterPayload);
+
+    bpf_ringbuf_submit (ev, 0);
+    return 0;
+  }
 }
 
 SEC ("tracepoint/raw_syscalls/sys_exit")
@@ -349,97 +309,187 @@ on_sys_exit (struct trace_event_raw_sys_exit * ctx)
     return 0;
 
   __s32 nr = (__s32) ctx->id;
-  SyscallEventKind kind = classify_syscall (nr);
 
-  __u16 payload_len = 0;
+  Inflight * in = bpf_map_lookup_elem (&inflight, &tid);
+  if (in != NULL && in->kind == INFLIGHT_KIND_OUT_COPY && in->syscall_nr == nr)
+  {
+    SyscallExitEventOut * ev = reserve_exit_out ();
+    if (ev == NULL)
+      return 0;
 
-  if (kind == SYSCALL_EVENT_READLINKAT)
-    payload_len = (__u16) sizeof (SyscallReadlinkatExitPayload);
+    fill_common (&ev->common, tgid, tid, nr, SYSCALL_PHASE_EXIT, ctx);
 
-  SyscallEvent * e = reserve_event (kind, payload_len);
-  if (e == NULL)
+    ev->payload.retval = (__s64) ctx->ret;
+
+    long n = ctx->ret;
+    if (n > 0)
+    {
+      __u32 maxn = in->u.out_copy.max_len;
+      if (maxn > MAX_PATH - 1)
+        maxn = MAX_PATH - 1;
+
+      __u32 to_copy = (__u32) n;
+      if (to_copy > maxn)
+        to_copy = maxn;
+
+      ev->attach.type = ATTACH_OUT_BYTES;
+      ev->attach.arg_index = in->u.out_copy.arg_index;
+
+      __u16 used;
+      if (to_copy == (MAX_PATH - 1))
+      {
+        ev->attach.len = (MAX_PATH - 1) + 1;
+        bpf_probe_read_user (&ev->data[0], MAX_PATH - 1, (void *) in->u.out_copy.user_ptr);
+        ev->data[MAX_PATH - 1] = '\0';
+        used = (__u16) MAX_PATH;
+      }
+      else
+      {
+        ev->attach.len = to_copy + 1;
+        if (to_copy != 0)
+          bpf_probe_read_user (&ev->data[0], to_copy, (void *) in->u.out_copy.user_ptr);
+        ev->data[to_copy] = '\0';
+        used = (__u16) (to_copy + 1);
+      }
+
+      ev->common.payload_len = ( __u16 ) (sizeof (SyscallExitPayload) + sizeof (AttachHeader) + used);
+      ev->common.attach_count = 1;
+    }
+    else
+    {
+      ev->common.payload_len = ( __u16 ) sizeof (SyscallExitPayload);
+    }
+
+    bpf_map_delete_elem (&inflight, &tid);
+
+    bpf_ringbuf_submit (ev, 0);
     return 0;
+  }
 
+  {
+    SyscallExitEventNone * ev = reserve_exit_none ();
+    if (ev == NULL)
+      return 0;
+
+    fill_common (&ev->common, tgid, tid, nr, SYSCALL_PHASE_EXIT, ctx);
+
+    ev->payload.retval = (__s64) ctx->ret;
+
+    ev->common.payload_len = ( __u16 ) sizeof (SyscallExitPayload);
+
+    bpf_ringbuf_submit (ev, 0);
+    return 0;
+  }
+}
+
+static __always_inline SyscallEnterEventNone *
+reserve_enter_none (void)
+{
+  return bpf_ringbuf_reserve (&events, sizeof (SyscallEnterEventNone), 0);
+}
+
+static __always_inline SyscallEnterEventPath *
+reserve_enter_path (void)
+{
+  return bpf_ringbuf_reserve (&events, sizeof (SyscallEnterEventPath), 0);
+}
+
+static __always_inline SyscallEnterEventSock *
+reserve_enter_sock (void)
+{
+  return bpf_ringbuf_reserve (&events, sizeof (SyscallEnterEventSock), 0);
+}
+
+static __always_inline SyscallExitEventNone *
+reserve_exit_none (void)
+{
+  return bpf_ringbuf_reserve (&events, sizeof (SyscallExitEventNone), 0);
+}
+
+static __always_inline SyscallExitEventOut *
+reserve_exit_out (void)
+{
+  return bpf_ringbuf_reserve (&events, sizeof (SyscallExitEventOut), 0);
+}
+
+static __always_inline void
+fill_common (SyscallEventCommon * e, __u32 tgid, __u32 tid, __s32 nr, SyscallPhase phase, void * ctx)
+{
   e->time_ns = bpf_ktime_get_ns ();
   e->tgid = tgid;
   e->tid = tid;
-  e->syscall_nr = nr;
-  e->kind = kind;
-  e->payload_len = payload_len;
 
-  e->phase = SYSCALL_PHASE_EXIT;
-  e->retval = (__s64) ctx->ret;
+  e->syscall_nr = nr;
+  e->phase = phase;
+
+  e->payload_len = 0;
+  e->attach_count = 0;
 
   e->stack_id = bpf_get_stackid (ctx, &stacks, BPF_F_USER_STACK);
+}
 
-  if (kind == SYSCALL_EVENT_READLINKAT)
+static __always_inline void
+fill_enter_args (SyscallEnterPayload * p, struct trace_event_raw_sys_enter * ctx)
+{
+  p->args[0] = (__u64) ctx->args[0];
+  p->args[1] = (__u64) ctx->args[1];
+  p->args[2] = (__u64) ctx->args[2];
+  p->args[3] = (__u64) ctx->args[3];
+  p->args[4] = (__u64) ctx->args[4];
+  p->args[5] = (__u64) ctx->args[5];
+}
+
+static __always_inline __u16
+write_attach_str_arg (AttachHeader * h, __u16 arg_index, __u8 * dst, __u32 dst_cap, const void * user_str)
+{
+  h->type = ATTACH_STRING_ARG;
+  h->arg_index = arg_index;
+  h->len = 0;
+
+  long r = bpf_probe_read_user_str (dst, dst_cap, user_str);
+  __u32 used = (r > 0) ? (__u32) r : 0;
+
+  h->len = used;
+  return (__u16) used;
+}
+
+static __always_inline __u16
+write_attach_bytes_arg (AttachHeader * h, __u16 arg_index, __u8 * dst, __u32 dst_cap, const void * user_src, __u32 n)
+{
+  h->type = ATTACH_BYTES_ARG;
+  h->arg_index = arg_index;
+
+  if (n > dst_cap)
   {
-    SyscallReadlinkatExitPayload * p = (void *) (e + 1);
-
-    p->link_len = 0;
-
-    ReadlinkatArgs * a = bpf_map_lookup_elem (&readlinkat_args, &tid);
-    if (a != NULL)
-    {
-      long n = ctx->ret;
-      if (n > 0)
-      {
-        __u32 maxn = a->bufsize;
-        if (maxn > (MAX_PATH - 1))
-          maxn = (MAX_PATH - 1);
-
-        __u32 to_copy = (__u32) n;
-        if (to_copy > maxn)
-          to_copy = maxn;
-
-        bpf_probe_read_user (p->link, to_copy, (void *) a->buf);
-        p->link[to_copy] = '\0';
-        p->link_len = to_copy + 1;
-      }
-
-      bpf_map_delete_elem (&readlinkat_args, &tid);
-    }
+    h->len = dst_cap;
+    if (dst_cap != 0)
+      bpf_probe_read_user (dst, dst_cap, user_src);
+    return (__u16) dst_cap;
   }
 
-  bpf_ringbuf_submit (e, 0);
-  return 0;
+  h->len = n;
+  if (n != 0)
+    bpf_probe_read_user (dst, n, user_src);
+  return (__u16) n;
 }
 
-static __always_inline SyscallEvent *
-reserve_event (SyscallEventKind kind, __u16 payload_len)
+static __always_inline void
+maybe_schedule_out_copy (__u32 tid, __s32 nr, __u16 arg_index, __u64 user_ptr, __u32 max_len)
 {
-  __u32 total_len = sizeof (SyscallEvent) + (__u32) payload_len;
-  return bpf_ringbuf_reserve (&events, total_len, 0);
-}
+  Inflight v;
 
-static __always_inline SyscallEventKind
-classify_syscall (__s32 nr)
-{
-  switch (nr)
-  {
-    case FRIDA_LINUX_SYSCALL_OPENAT:
-      return SYSCALL_EVENT_OPENAT;
-    case FRIDA_LINUX_SYSCALL_FACCESSAT:
-      return SYSCALL_EVENT_FACCESSAT;
-    case FRIDA_LINUX_SYSCALL_STATFS:
-      return SYSCALL_EVENT_STATFS;
-#ifdef FRIDA_LINUX_SYSCALL_NEWFSTATAT
-    case FRIDA_LINUX_SYSCALL_NEWFSTATAT:
-      return SYSCALL_EVENT_NEWFSTATAT;
-#endif
-    case FRIDA_LINUX_SYSCALL_READLINKAT:
-      return SYSCALL_EVENT_READLINKAT;
-    case FRIDA_LINUX_SYSCALL_CONNECT:
-      return SYSCALL_EVENT_CONNECT;
-    default:
-      return SYSCALL_EVENT_GENERIC;
-  }
-}
+  v.syscall_nr = nr;
+  v.kind = INFLIGHT_KIND_OUT_COPY;
+  v._pad0 = 0;
 
-static __always_inline __u32
-clamp_u32 (__u32 v, __u32 max)
-{
-  return (v > max) ? max : v;
+  v.u.out_copy.arg_index = arg_index;
+  v.u.out_copy._pad1 = 0;
+
+  v.u.out_copy.user_ptr = user_ptr;
+  v.u.out_copy.max_len = max_len;
+  v.u.out_copy._pad2 = 0;
+
+  bpf_map_update_elem (&inflight, &tid, &v, BPF_ANY);
 }
 
 char LICENSE[] SEC ("license") = "Dual BSD/GPL";
